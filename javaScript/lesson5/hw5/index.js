2fca1cdde8f9ad46074eadf134e41ce92f900bd5
Ksondzyk <roman.kson@gmail.com>
2020-03-21 14:22:45 +0200

    push123

--- javaScript/lesson5/hw5/index.js
+++ javaScript/lesson5/hw5/index.js
@@ -3,11 +3,12 @@ function findDivCount(a, b, n) {
   for (let i = a; i <= b; i++) {
     sum = i;
     if (sum % n === 0) {
-      alert(sum);
-    } else {
+      console.log(sum);
+    } else if (a > 12 && b < 120) {
       return 0;
     }
   }
+  return 0;
 }
 
 findDivCount(12, 120, 16);
