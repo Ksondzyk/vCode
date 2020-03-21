a0c12293c4d79c764fd43f66038ec4410c3abdd5
Ksondzyk <roman.kson@gmail.com>
2020-03-21 19:40:00 +0200

    Prime numbers

--- javaScript/lesson5/hw2/index.js
+++ javaScript/lesson5/hw2/index.js
@@ -2,8 +2,8 @@ function getPrimes(n) {
   nextPrime: for (let i = 2; i <= n; i++) {
     for (let j = 2; j < i; j++) {
       if (i % j == 0) continue nextPrime;
-      console.log(i);
     }
+    console.log(i);
   }
 }
 getPrimes(18);
