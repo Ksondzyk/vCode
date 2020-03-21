b7d85344a203b33486c0851b66e76d7b0b08f1cd
Ksondzyk <roman.kson@gmail.com>
2020-03-21 13:45:12 +0200

    a in indx

--- javaScript/lesson5/hw4/index.js
+++ javaScript/lesson5/hw4/index.js
@@ -1,12 +1,12 @@
 function increaser(a, index) {
-  for (var i = 0; i < a; i++) {
+ 
  
   if (a < index) {
     return a + index;
   } else {
     return a;
   }
-}
+
 
 const result = increaser(12, 7);
 console.log(result);
