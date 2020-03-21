3816812973a118d51dc99a9f55840d1f466f2af7
Ksondzyk <roman.kson@gmail.com>
2020-03-21 15:54:03 +0200

    reload12

--- javaScript/lesson5/hw5/index.js
+++ javaScript/lesson5/hw5/index.js
@@ -2,8 +2,7 @@ function findDivCount(a, b, n) {
   let sum = 0;
   for (let i = a; i <= b; i++) {
     if (i % n === 0) {
-      sum = i;
-      alert(sum);
+      sum += 1;
     }
   }
   return sum;
