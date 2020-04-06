7d12b50779e843d78167164a4d6278f5fb3a4359
Ksondzyk <roman.kson@gmail.com>
2020-04-06 13:09:53 +0300

    reload

--- javaScript/lesson6/task1/index.js
+++ javaScript/lesson6/task1/index.js
@@ -9,11 +9,11 @@
 // [11, 22, 33, 55, 66]=>[length, first elem , last elem]
 
 const numbers = [11, 22, 33, 55, 66];
-function getArrayBound(arr) {
+function getArrayBounds(arr) {
   if (Array.isArray(arr)) {
     return null;
   }
   return [arr.length, arr[0], arr[arr.length - 1]];
 }
-const arrayBound = getArrayBound(numbers);
+const arrayBound = getArrayBounds(numbers);
 console.log(arrayBound);
