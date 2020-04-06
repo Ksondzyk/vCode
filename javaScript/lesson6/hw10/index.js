a4361464449d9ff66b9e4aa3b748ec5cc64e0913
Ksondzyk <roman.kson@gmail.com>
2020-04-06 18:39:13 +0300

    reload

--- javaScript/lesson6/hw10/index.js
+++ javaScript/lesson6/hw10/index.js
@@ -1,14 +1,26 @@
 const arr = [1, 2, 3, 4, 5];
 
+// function getSubArray(array, number) {
+//   let firstNumbers = [];
+//   for (let elem of array) {
+//     if (elem <= number) {
+//       firstNumbers.push(elem);
+//     }
+//   }
+//   return firstNumbers;
+// }
+
+// const result = getSubArray(arr, 4);
+// console.log(result);
+
 function getSubArray(array, number) {
   let firstNumbers = [];
-  for (let elem of array) {
-    if (elem <= number) {
-      firstNumbers.push(elem);
+  array.forEach(function (item, index) {
+    if (index < number) {
+      firstNumbers.push(item);
     }
-  }
+  });
   return firstNumbers;
 }
-
 const result = getSubArray(arr, 4);
 console.log(result);
