770086d1b9feb8c771985319a61bc60080360c87
Ksondzyk <roman.kson@gmail.com>
2020-04-06 16:56:59 +0300

    reverce

--- javaScript/lesson6/hw5/index.js
+++ javaScript/lesson6/hw5/index.js
@@ -1,9 +1,12 @@
+const arr = [1, 5, 6, 8, 9];
 function reverseArray(numbers) {
   if (!Array.isArray(numbers)) {
     return null;
   }
-  let reverse = numbers.reverse();
-  return reverse;
+  let reverse = [...numbers];
+  let value = reverse.reverse();
+  return value;
 }
-const result = reverseArray([1, 5, 6, 8, 9]);
+const result = reverseArray(arr);
+console.log(arr);
 console.log(result);
