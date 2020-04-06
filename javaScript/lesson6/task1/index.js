5567983aea7928425606fe37a5b054e894937676
Ksondzyk <roman.kson@gmail.com>
2020-04-06 13:03:14 +0300

    arrays

---
+++ javaScript/lesson6/task1/index.js
@@ -0,0 +1,18 @@
+// const numbers = [11, 22, 33, 55, 66];
+
+// const numberofElements = numbers.length;
+// console.log(numberofElements);
+
+// console.log("Element #1:" + numbers[1]);
+// console.log("Element last:" + numbers[numbers.length - 1]);
+
+// [11, 22, 33, 55, 66]=>[length, first elem , last elem]
+const numbers = [11, 22, 33, 55, 66];
+function getArrayBound(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  return [arr.length, arr[0], arr[arr.length - 1]];
+}
+const arrayBound = getArrayBound(1);
+console.log(arrayBound);
