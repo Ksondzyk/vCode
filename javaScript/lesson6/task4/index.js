5fa27c3060f120368e57860f97a1d1450ff88aa6
Ksondzyk <roman.kson@gmail.com>
2020-04-06 15:24:28 +0300

    Array destructuring and spread prerator

---
+++ javaScript/lesson6/task4/index.js
@@ -0,0 +1,15 @@
+const numbers = [11, 22, 33, 55, 66];
+
+// const a = numbers[0];
+// const b = numbers[1];
+
+const [a, b, ...rest] = numbers;
+
+console.log(a, b, [1, 2, 3, ...rest]);
+
+function swap(arr) {
+  const [start, ...rest] = arr;
+  return [...rest, start];
+}
+
+console.log(swap(numbers));
