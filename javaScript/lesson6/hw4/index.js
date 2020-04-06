ac943a776b671e1d1f987361eb9e1d9d0253bc70
Ksondzyk <roman.kson@gmail.com>
2020-04-06 16:44:07 +0300

    Even increase

---
+++ javaScript/lesson6/hw4/index.js
@@ -0,0 +1,16 @@
+function increaseEvenEl(arr, delta) {
+  let evenNumbers = [];
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  for (let number of arr) {
+    if (number % 2 === 0) {
+      evenNumbers.push(number + delta);
+    } else if (number % 2 == 1) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
+}
+const result = increaseEvenEl([2, 5, 6, 8, 11, 9, 4], 20);
+console.log(result);
