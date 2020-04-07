92c5657f7fa67314d0dcbe7409806db4d932b2ab
Ksondzyk <roman.kson@gmail.com>
2020-04-07 11:23:51 +0300

    reload

--- javaScript/lesson7/task1/index.js
+++ javaScript/lesson7/task1/index.js
@@ -1,6 +1,3 @@
-const getSpecialNumbers = (numbers) => {
-  return (result = numbers.filter((num) => num % 3 === 0));
-};
-
+const getSpecialNumbers = (numbers) => numbers.filter((num) => num % 3 === 0);
 const arr = [1, 2, 3, 4, 5, 6, 18];
 console.log(getSpecialNumbers(arr));
