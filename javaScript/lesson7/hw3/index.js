655ca2a3401e784d44033752e6a43cd7111ee93a
Ksondzyk <roman.kson@gmail.com>
2020-04-07 15:25:29 +0300

    reload

--- javaScript/lesson7/hw3/index.js
+++ javaScript/lesson7/hw3/index.js
@@ -1,15 +1,12 @@
 const array = [2, 5, 6, 8, 11, 9, 4];
 
 const increaseEvenEl = (arr, delta) => {
-  const increaseEvenNumbers = [];
   if (!Array.isArray(arr)) {
     return null;
   }
-  arr.map((number) => {
-    number % 2 === 0
-      ? increaseEvenNumbers.push(number + delta)
-      : increaseEvenNumbers.push(number);
-  });
-  return increaseEvenNumbers;
+  const result = arr.map((number) =>
+    number % 2 === 0 ? number + delta : number
+  );
+  return result;
 };
 console.log(increaseEvenEl(array, 20));
