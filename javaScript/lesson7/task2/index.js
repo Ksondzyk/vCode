a881e39798ab0c1c29073e79a12236e6883b2998
Ksondzyk <roman.kson@gmail.com>
2020-04-07 12:01:05 +0300

    reload

--- javaScript/lesson7/task2/index.js
+++ javaScript/lesson7/task2/index.js
@@ -1,2 +1,4 @@
-const sortArray = (numbers) => numbers.slice().sort((a, b) => b - a);
-console.log(sortArray([3, 1, 2, 88, 15, 12]));
+const arr = [3, 1, 2, 88, 15, 12];
+const sortDesc = (numbers) => numbers.slice().sort((a, b) => b - a);
+
+console.log(sortDesc(arr));
