ef02c65a8bdebf2cf702741f64b4128f456d774e
Ksondzyk <roman.kson@gmail.com>
2020-04-08 14:48:41 +0300

    obj

---
+++ javaScript/lesson8/task1/index.js
@@ -0,0 +1,26 @@
+// const prop = "my";
+// const obj = {
+//   name: "Tom",
+//   age: 17,
+//   inderest: "football",
+//   "my name": "Bob",
+//   [`${prop}name`]: "value",
+// };
+
+// obj.age = 18;
+// console.log(obj.age);
+// console.log(obj["my name"]);
+// console.log(obj[`${prop}name`]);
+
+const transformToObject = (arr) => {
+  const obj = {};
+
+  arr.forEach((element) => {
+    obj[element] = element;
+  });
+  return obj;
+};
+
+const arr = ["a", 17.1, "John Doe"];
+const result = transformToObject(arr);
+console.log(result);
