95bf8684d2e84f2959e36c0bd6306506dd4ef0d8
Ksondzyk <roman.kson@gmail.com>
2020-04-09 16:09:29 +0300

    Retrieving properties from an object

---
+++ javaScript/lesson8/hw9/index.js
@@ -0,0 +1,16 @@
+const pickProps = (arg1, arg2) => {
+  const arr2 = Object.keys(arg1);
+  const res = arg2.reduce((acc, prevValue, nextValue) => {
+    if (prevValue === arr2[nextValue]) {
+    }
+    return { ...acc, [prevValue]: arr2[nextValue] };
+  }, {});
+  return res;
+};
+
+const result = pickProps({ a: "a", "17.1": 17.1, "John Doe": "John Doe" }, [
+  "a",
+  17.1,
+  "John Doe",
+]);
+console.log(result);
