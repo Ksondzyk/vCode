7af205f3ee85b27be7ecc9e051ffc6823039a2ab
Ksondzyk <roman.kson@gmail.com>
2020-04-09 13:58:44 +0300

    reload

--- javaScript/lesson8/hw8/index.js
+++ javaScript/lesson8/hw8/index.js
@@ -1,10 +1,11 @@
 const buildObject = (keysList, valuesList) => {
-const result =keysList.reduce((acc,prevValue,nextValue) => {
-    return { ...acc, [prevValue]:valuesList[nextValue]}
-})
-
-const result = buildObject(["name", "address", "age"], ["Bob", "Ukraine", 34]);
-console.log(result);
+  const result = keysList.reduce((acc, prevValue, nextValue) => {
+    return { ...acc, [prevValue]: valuesList[nextValue] };
+  });
+  return result;
+};
+const result1 = buildObject(["name", "address", "age"], ["Bob", "Ukraine", 34]);
+console.log(result1);
 
 // const keys = ['name', 'address', 'age'];
 // const values = ['Bob', 'Ukraine', 34];
