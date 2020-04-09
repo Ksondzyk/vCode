71f3a3589b5978790b06e61709efd832a225efdb
Ksondzyk <roman.kson@gmail.com>
2020-04-09 23:48:15 +0300

    reload

--- javaScript/lesson8/hw7/index.js
+++ javaScript/lesson8/hw7/index.js
@@ -1,4 +1,22 @@
-export const compareObjects = (obj1, obj4) => {
+            const obj1 = {
+                name: 'Tom'
+                age: 17
+            };
+            const obj2 = {
+                name: 'Bob'
+                age: 17
+            };
+            const obj3 = {
+                name: 'Bob'
+                age: 17,
+                student: false
+            };
+            const obj4 = {
+                name: 'Tom'
+                age: 17,
+            };
+
+function compareObjects (obj1, obj4) {
   const arr1 = Object.keys(obj1);
   for (const key of arr1) {
     if (obj1[key] !== obj4[key]) {
@@ -8,3 +26,9 @@ export const compareObjects = (obj1, obj4) => {
   }
   return false;
 };
+const result = compareObjects(obj1, obj2);
+console.log(result)
+const result1 = compareObjects(obj2, obj3);
+console.log(result1)
+const result2 = compareObjects(obj2, obj4);
+console.log(result2);
