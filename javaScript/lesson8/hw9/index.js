2646a7341aa71995b8ea457393081e1c47d5b631
Ksondzyk <roman.kson@gmail.com>
2020-04-09 22:37:36 +0300

    obj

--- javaScript/lesson8/hw9/index.js
+++ javaScript/lesson8/hw9/index.js
@@ -19,9 +19,6 @@ const obj = { a: "a", "17.1": 17.1, "John Doe": "John Doe" };
 const pickProps = (arg1, arg2) => {
   const arr2 = Object.keys(arg1);
   const res = arg2.reduce((acc, prevValue, nextValue) => {
-    if (prevValue !== arr2[nextValue]) {
-      return {};
-    }
     return { ...acc, [prevValue]: arr2[nextValue] };
   }, {});
   return res;
