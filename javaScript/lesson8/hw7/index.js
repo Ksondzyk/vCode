23fc1f44843591541ecc636c37f7cc17dcc27fc6
Ksondzyk <roman.kson@gmail.com>
2020-04-09 17:05:58 +0300

    reload

--- javaScript/lesson8/hw7/index.js
+++ javaScript/lesson8/hw7/index.js
@@ -15,25 +15,8 @@ const obj4 = {
   name: "Tom",
   age: 17,
 };
-const compareObjects1 = (obj1, obj2) => {
-  return obj1 === obj2;
-};
-const result = compareObjects1(obj1, obj2);
-console.log(result);
 
-const compareObjects2 = (obj2, obj3) => {
-  return obj2 === obj3;
-};
-const result1 = compareObjects2(obj2, obj3);
-console.log(result1);
-// @Roman Ksondzyk попробуй по этому алгоритму
-// 1. Создать ф-цию compareObjects
-// 2. Сравнить к-во ключей в объектах
-// 3. Если к-во ключей разное, то объекты разные, вернуть false
-// 4. Если к-во ключей одинаковое, то пройтись по всем ключам первого объекта и сравнить значение свойств двух объектов по каждому ключу
-// 5. Если для всех ключей значения свойств равны, то и объекты равны
-
-const compareObjects = (obj1, obj4) => {
+export const compareObjects = (obj1, obj4) => {
   const arr1 = Object.keys(obj1);
   for (const key of arr1) {
     if (obj1[key] !== obj4[key]) {
@@ -43,6 +26,3 @@ const compareObjects = (obj1, obj4) => {
   }
   return false;
 };
-
-const result4 = compareObjects(obj1, obj4);
-console.log(result4);
