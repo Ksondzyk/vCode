547ff75d89c8bcf074447d596ef2094e5b3d5160
Ksondzyk <roman.kson@gmail.com>
2020-04-09 17:12:37 +0300

    reload

--- javaScript/lesson8/hw7/index.js
+++ javaScript/lesson8/hw7/index.js
@@ -1,21 +1,3 @@
-// const obj1 = {
-//   name: "Tom",
-//   age: 17,
-// };
-// const obj2 = {
-//   name: "Bob",
-//   age: 17,
-// };
-// const obj3 = {
-//   name: "Bob",
-//   age: 17,
-//   student: false,
-// };
-// const obj4 = {
-//   name: "Tom",
-//   age: 17,
-// };
-
 export const compareObjects = (obj1, obj4) => {
   const arr1 = Object.keys(obj1);
   for (const key of arr1) {
