4bdba34bf890ec6ab71239ad7ece4e81fb1d4ac2
Ksondzyk <roman.kson@gmail.com>
2020-04-10 19:14:30 +0300

    object create

---
+++ javaScript/lesson9/hw1/index.js
@@ -0,0 +1,50 @@
+// const customers = {
+//   "customer-id-1": {
+//     name: "William",
+//     age: 54,
+//   },
+//   "customer-id-2": {
+//     name: "Tom",
+//     age: 17,
+//   },
+// };
+
+// const getCustomersList = (obj) => {
+//   const arrUsers = Object.keys(obj);
+//   const values = Object.values(obj);
+//   const newArr = [];
+//   values.reduce((acc, elm, index) => {
+//     return (acc = elm.id = arrUsers[index]);
+//   }, {});
+//   values.sort((a, b) => a.age - b.age);
+//   return values;
+// };
+
+// const result = getCustomersList(customers);
+// console.log(result);
+
+const customers = {
+  "customer-id-1": {
+    name: "William",
+    age: 54,
+  },
+  "customer-id-2": {
+    name: "Tom",
+    age: 17,
+  },
+};
+
+const getCustomersList = (obj) => {
+  const newObj = JSON.parse(JSON.stringify(obj));
+  const arrUsers = Object.keys(newObj);
+  const values = Object.values(newObj);
+  values.reduce((acc, elm, index) => {
+    return (acc = elm.id = arrUsers[index]);
+  }, {});
+  values.sort((a, b) => a.age - b.age);
+  return values;
+};
+
+const result = getCustomersList(customers);
+console.log(result);
+console.log(customers);
