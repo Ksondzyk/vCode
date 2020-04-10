26e2ebca5a9a095081c30e521082154c16c710e9
Ksondzyk <roman.kson@gmail.com>
2020-04-10 13:29:15 +0300

    reload

--- javaScript/lesson9/task1/index.js
+++ javaScript/lesson9/task1/index.js
@@ -1,8 +1,8 @@
 const obj1 = { "John Doe": 19, Tom: 17, Bob: 18 };
 const getAdults = (obj) => {
-  const usersArr = Object.entries(obj);
+  const usersAr = Object.entries(obj);
 
-  const filterUsersArr = usersArr.filter((user) => user[1] >= 18);
+  const filterUsersArr = usersAr.filter((user) => user[1] >= 18);
   const usersNames = filterUsersArr.map((user) => user[0]);
   return usersNames;
 };
