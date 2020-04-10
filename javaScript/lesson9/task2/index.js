2df52b5a13964410b7e874c8609c404bae599d2d
Ksondzyk <roman.kson@gmail.com>
2020-04-10 14:41:24 +0300

    copyObj

--- javaScript/lesson9/task2/index.js
+++ javaScript/lesson9/task2/index.js
@@ -2,11 +2,12 @@ const users = { Tom: 17, Bob: 100 };
 
 const players = { "John Doe": 19, Bob: 18 };
 
-const res = { ...players, ...users };
+// const res = { ...players, ...users };
+
+// console.log(players);
+// console.log(res);
+
+const copyObj = (obj) => ({ ...obj });
 
 console.log(players);
-console.log(res);
-
-// const copyObj = (obj) => ({ ...obj });
-
-// console.log(copyObj(users ));
+console.log(copyObj(users));
