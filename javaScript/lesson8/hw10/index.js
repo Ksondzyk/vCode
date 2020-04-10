8714998429d7c7b4d155ae6a7db5bbfacab4e739
Ksondzyk <roman.kson@gmail.com>
2020-04-11 00:50:16 +0300

    reload

--- javaScript/lesson8/hw10/index.js
+++ javaScript/lesson8/hw10/index.js
@@ -1,15 +1,21 @@
-const obj = { "John Doe": 19, Tom: 17, Bob: 18 };
+// const obj = { "John Doe": 19, Tom: 17, Bob: 18 };
 
-const getAdults = (arrObj) => {
-  if (getAdults === undefined) {
-    return {};
-  }
-  return Object.keys(arrObj).reduce((acc, el) => {
-    if (arrObj[el] >= 18) {
-      acc[el] = arrObj[el];
-    }
-    return acc;
-  }, {});
-};
-const result = getAdults(obj);
-console.log(result);
+// const getAdults = (arrObj) => {
+//   if (getAdults === undefined) {
+//     return {};
+//   }
+//   return Object.keys(arrObj).reduce((acc, el) => {
+//     if (arrObj[el] >= 18) {
+//       acc[el] = arrObj[el];
+//     }
+//     return acc;
+//   }, {});
+// };
+// const result = getAdults(obj);
+// console.log(result);
+const people = { "John Doe": 19, Tom: 17, Bob: 18 };
+
+const getAdults = (arr) =>
+  Object.fromEntries(Object.entries(arr).filter((k) => k[1] >= 18));
+const res = getAdults(people);
+console.log(res);
