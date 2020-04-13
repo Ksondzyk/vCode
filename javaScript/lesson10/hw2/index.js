eed060c0ab772eb35af888d08c61fa3eddf4e57b
Ksondzyk <roman.kson@gmail.com>
2020-04-13 20:05:07 +0300

    rel

--- javaScript/lesson10/hw2/index.js
+++ javaScript/lesson10/hw2/index.js
@@ -11,5 +11,5 @@ const getRandomNumbers = (number, ...args) => {
   }
   return newArr;
 };
-const result = getRandomNumbers(5, 12, 24);
+const result = getRandomNumbers(5, -12, -24);
 console.log(result);
