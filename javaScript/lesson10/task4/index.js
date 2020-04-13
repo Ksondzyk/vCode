17bc66206d6b37ae986b755b897e9086af5e4213
Ksondzyk <roman.kson@gmail.com>
2020-04-13 15:50:48 +0300

    max Number

--- javaScript/lesson10/task4/index.js
+++ javaScript/lesson10/task4/index.js
@@ -1,4 +1,7 @@
 const getMaxAbsoluteNumber = (arr) => {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return null;
+  }
   return Math.abs(...arr);
 };
 console.log(getMaxAbsoluteNumber([-777, 3, -1, 45, -20]));
