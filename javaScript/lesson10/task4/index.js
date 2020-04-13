7dac282d3df712f34391e37787550dfdc1a8bb3a
Ksondzyk <roman.kson@gmail.com>
2020-04-13 17:27:26 +0300

    reload

--- javaScript/lesson10/task4/index.js
+++ javaScript/lesson10/task4/index.js
@@ -2,6 +2,6 @@ const getMaxAbsoluteNumber = (arr) => {
   if (!Array.isArray(arr) || arr.length === 0) {
     return null;
   }
-  return Math.abs(...arr);
+  return Math.max(...arr);
 };
 console.log(getMaxAbsoluteNumber([-777, 3, -1, 45, -20]));
