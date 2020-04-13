b1c10d25560fca59396854203d790e5d1f182691
Ksondzyk <roman.kson@gmail.com>
2020-04-13 19:58:37 +0300

    reload

--- javaScript/lesson10/hw2/index.js
+++ javaScript/lesson10/hw2/index.js
@@ -14,6 +14,6 @@ const getRandomNumbers = (number, ...args) => {
 const result = getRandomNumbers(
   5,
   Math.floor(Math.random() * Math.floor(12)),
-  Math.random() * (24 - 13) + 13
+  Math.random() * (24 - 12) + 12
 );
 console.log(result);
