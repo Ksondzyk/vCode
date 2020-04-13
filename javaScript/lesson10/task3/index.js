c7fc10f2581cfad84862362dba03e40c94485489
Ksondzyk <roman.kson@gmail.com>
2020-04-13 20:23:15 +0300

    rel

--- javaScript/lesson10/task3/index.js
+++ javaScript/lesson10/task3/index.js
@@ -1,9 +1,10 @@
-const multiRound = (...num) => [
-  Math.round(num),
-  Math.floor(num),
-  Math.ceil(num),
-  Math.trunc(num),
-  Math.Number(num.toFixed(3)),
-];
-
+const multiRound = (...num) => {
+  return [
+    Math.round(num),
+    Math.floor(num),
+    Math.ceil(num),
+    Math.trunc(num),
+    Math.Number(num.toFixed(3)),
+  ];
+};
 console.log(multiRound(17.3433));
