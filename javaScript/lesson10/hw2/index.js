d4e3fed11f5f687f0320e2aa5355d3955dc2ba4f
Ksondzyk <roman.kson@gmail.com>
2020-04-13 19:53:58 +0300

    reload

--- javaScript/lesson10/hw2/index.js
+++ javaScript/lesson10/hw2/index.js
@@ -2,13 +2,18 @@ const getRandomNumbers = (number, ...args) => {
   if (args[1] > args[2] || (args[1] < 0 && args[2] < 0)) {
     return null;
   }
-
-  const res = args.map((elem) => number && Math.round(elem));
-  return res;
+  const newArr = [];
+  for (let i = 0; i < number; i++) {
+    const res = Math.random() * (args[1] - args[0]) + args[0];
+    const el = Math.round(res);
+    debugger;
+    newArr.push(el);
+  }
+  return newArr;
 };
 const result = getRandomNumbers(
-  2,
-  Math.random(4.12),
-  Math.random() * (12.4 - 4.12) + 4.12
+  5,
+  Math.floor(Math.random() * Math.floor(12)),
+  Math.random() * (24 - 13) + 13
 );
 console.log(result);
