147cf4302f3474f8aedc77bb7917d3af7f26511f
Ksondzyk <roman.kson@gmail.com>
2020-04-14 19:47:36 +0300

    rel

--- javaScript/lesson11/hw1/index.js
+++ javaScript/lesson11/hw1/index.js
@@ -2,21 +2,9 @@ const splitString = (text, len) => {
   if (typeof text !== "string") {
     return null;
   }
-  if (len === undefined) {
-    const strArr = [];
-    let startPosition = 0;
-    while (true) {
-      const chunk = text.substr(startPosition, 10);
-      if (chunk.length === 0) {
-        break;
-      }
-      strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
-      startPosition += 10;
-    }
-    return strArr.join("\n");
-  }
-  const strArr = [];
+
   let startPosition = 0;
+  const strArr = [];
   while (true) {
     const chunk = text.substr(startPosition, len);
     if (chunk.length === 0) {
