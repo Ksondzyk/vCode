86d36b3a66d01beeeaac1870204f62383eaf8c20
Ksondzyk <roman.kson@gmail.com>
2020-04-14 11:04:10 +0300

    reload

--- javaScript/lesson10/hw3/index.js
+++ javaScript/lesson10/hw3/index.js
@@ -1,3 +1,12 @@
-const superRound = (arg1) => {};
+const superRound = (arg1, arg2) => {
+  return [
+    +Math.floor(arg1).toFixed(arg2),
+    +Math.round(arg1).toFixed(arg2),
+    +Math.ceil(arg1).toFixed(arg2),
+    +Math.trunc(arg1).toFixed(arg2),
+    Number(arg1.toFixed(arg2)),
+  ];
+};
 
-const result = superRound(12.172);
+const result = superRound(12.17244444, 3);
+console.log(result);
