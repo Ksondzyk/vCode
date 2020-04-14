e6c306f120ab24d5542b60a10e84ac9142777738
Ksondzyk <roman.kson@gmail.com>
2020-04-14 10:05:02 +0300

    reload

--- javaScript/lesson10/task3/index.js
+++ javaScript/lesson10/task3/index.js
@@ -1,10 +1,10 @@
-const multiRound = (num) => {
+const multiRound = (num, num2) => {
   return [
-    Math.round(num),
-    Math.floor(num),
-    Math.ceil(num),
-    Math.trunc(num),
-    Number(num.toFixed()),
+    Math.round(num, num2),
+    Math.floor(num, num2),
+    Math.ceil(num, num2),
+    Math.trunc(num, num2),
+    Number(num.toFixed(2)),
   ];
 };
-console.log(multiRound(17.3433));
+console.log(multiRound(17.3433, -17.3433));
