3e70cf8e28313b3d7c1b80ace001bc93c04abd38
Ksondzyk <roman.kson@gmail.com>
2020-04-14 17:55:36 +0300

    reload

--- javaScript/lesson11/task1/index.js
+++ javaScript/lesson11/task1/index.js
@@ -2,6 +2,19 @@ const splitText = (text, len) => {
   if (typeof text !== "string") {
     return null;
   }
+  if (len === undefined) {
+    const strArr = [];
+    let startPosition = 0;
+    while (true) {
+      const chunk = text.substr(startPosition, 10);
+      if (chunk.length === 0) {
+        break;
+      }
+      strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
+      startPosition += 10;
+    }
+    return strArr.join("\n");
+  }
   const strArr = [];
   let startPosition = 0;
   while (true) {
@@ -14,7 +27,7 @@ const splitText = (text, len) => {
   }
   return strArr.join("\n");
 };
-const result = splitText("abcdefgh", 4);
+const result = splitText("abcdefghasdfdsafsdf", 4);
 console.log(result);
 
 // разделить строку на масив через пробел
