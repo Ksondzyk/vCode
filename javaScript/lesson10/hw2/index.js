fe0d0e75a474c01468ef305afdeaa5ff2a9342b2
Ksondzyk <roman.kson@gmail.com>
2020-04-14 12:45:38 +0300

    reload

--- javaScript/lesson10/hw2/index.js
+++ javaScript/lesson10/hw2/index.js
@@ -1,10 +1,10 @@
-const getRandomNumbers = (number, ...args) => {
-  if (args[0] > args[1] || (args[0] < 0 && args[1] < 0)) {
-    return null;
-  }
+const getRandomNumbers = (number, arg1, arg2) => {
+  // if (args[0] > args[1] || (args[0] < 0 && args[1] < 0)) {
+  //   return null;
+  // }
   const newArr = [];
   for (let i = 0; i < number; i++) {
-    const res = Math.random() * (args[1] - args[0] - 1) + args[0] - 1;
+    const res = Math.random() * (arg2 - arg1) + arg1;
     const el = Math.round(res);
 
     newArr.push(el);
