b96779c91518b4014705af2f02558c7e10d0c2c5
Ksondzyk <roman.kson@gmail.com>
2020-04-14 12:06:01 +0300

    reload

--- javaScript/lesson10/hw3/index.js
+++ javaScript/lesson10/hw3/index.js
@@ -1,12 +1,12 @@
 const superRound = (arg1, arg2) => {
   return [
-    Math.floor(arg1 * 100) / 100,
-    Math.round(arg1 * 100) / 100,
-    Math.ceil(arg1 * 100) / 100,
-    Math.trunc(arg1 * 100) / 100,
+    Math.floor(arg1 * 10000) / 10000,
+    Math.round(arg1 * 10000) / 10000,
+    Math.ceil(arg1 * 10000) / 10000,
+    Math.trunc(arg1 * 10000) / 10000,
     Number(arg1.toFixed(arg2)),
   ];
 };
 
-const result = superRound(12.17244444, 2);
+const result = superRound(12.17244444, 4);
 console.log(result);
