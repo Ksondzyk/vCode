3f7e18920e9da0333f53388a6cede530b3649916
Ksondzyk <roman.kson@gmail.com>
2020-04-14 21:41:22 +0300

    Split a string into substrings

--- javaScript/lesson11/hw1/index.js
+++ javaScript/lesson11/hw1/index.js
@@ -2,18 +2,27 @@ const splitString = (text, len) => {
   if (typeof text !== "string") {
     return null;
   }
-
-  let startPosition = 0;
+  const dot = ".";
   const strArr = [];
+  let startPosition = 0;
+  let res = "";
   while (true) {
     const chunk = text.substr(startPosition, len);
     if (chunk.length === 0) {
       break;
     }
-    strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
+    if (chunk.length < len) {
+      const cycle = len - chunk.length;
+      //   res = chunk.substr(chunk);
+      for (let i = 0; i < cycle; i++) {
+        res += dot;
+        console.log(res);
+      }
+    }
+    strArr.push(chunk[0].toUpperCase() + chunk.slice(1) + res.substr());
     startPosition += len;
   }
   return strArr;
 };
-const result = splitString("abcdefghasdfdsafsdf", 4);
+const result = splitString("abcdefghasdfdsafsd", 4);
 console.log(result);
