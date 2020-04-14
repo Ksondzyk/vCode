500efab0884b6e60fd3dde9373859c64a469a8bd
Ksondzyk <roman.kson@gmail.com>
2020-04-14 22:38:06 +0300

    reload

--- javaScript/lesson11/hw1/index.js
+++ javaScript/lesson11/hw1/index.js
@@ -15,7 +15,7 @@ const splitString = (text, len) => {
         const cycle = 10 - chunk.length;
         res += ".".repeat(cycle);
       }
-      strArr.push(chunk + chunk.slice(1) + res.substr());
+      strArr.push(chunk + res.substr());
       startPosition += 10;
     }
     return strArr;
@@ -32,9 +32,9 @@ const splitString = (text, len) => {
       const cycle = len - chunk.length;
       res += ".".repeat(cycle);
     }
-    strArr.push(chunk + chunk.slice(1) + res.substr());
+    strArr.push(chunk + res.substr());
     startPosition += len;
   }
   return strArr;
 };
-console.log(splitString("abcd efgh", 4));
+console.log(splitString("abcdefgh", 4));
