1d477e7fd37fd11d572bdc160c4de959e269318d
Ksondzyk <roman.kson@gmail.com>
2020-04-14 15:06:22 +0300

    reload

--- javaScript/lesson10/hw2/index.js
+++ javaScript/lesson10/hw2/index.js
@@ -14,28 +14,15 @@
 // };
 // getRandomNumbers(100, -0.1, 0.1);
 
-// const getRandomNumbers = (number, to, from) => {
-//   if (to > from || (to < 0 && from < 0)) {
-//     return null;
-//   }
-//   return Array(number)
-//     .fill()
-//     .map(() => from + (to - from) * Math.random())
-//     .map((num) => (num < 0 ? Math.ceil(num) : Math.floor(num)));
-// };
-// const result = getRandomNumbers(100, -0.1, 0.1);
-// console.log(result);
-
-
-const getRandomNumbers = (len, a, b) => {
-  if (a > b) return null;
-  a = Math.ceil(a);
-  b = Math.floor(b);
-  let rand = Math.floor(Math.random() * (b - a + 1) + a);
-  if (rand > b) return null;
-  let arr = [];
-  for (let i = 0; i < len; i++) {
-    arr.push(Math.floor(Math.random() * (b - a + 1) + a));
+const getRandomNumbers = (number, to, from) => {
+  if (to > from || (to < 0 && from < 0)) {
+    return null;
   }
-  return arr;
+  return Array(number)
+    .fill()
+    .map(() => from + (to - from) * Math.random())
+    .map((num) => (num < 0 ? Math.ceil(num) : Math.floor(num)));
 };
+const result = getRandomNumbers(100, -0.1, 0.1);
+console.log(result);
+
