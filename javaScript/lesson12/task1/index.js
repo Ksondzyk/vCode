fef57dd8763d8c75438e9aeb1ce7b09a29813a82
Ksondzyk <roman.kson@gmail.com>
2020-04-15 12:14:14 +0300

    reload

---
+++ javaScript/lesson12/task1/index.js
@@ -0,0 +1,20 @@
+const calc = (expression) => {
+  const [a, operator, b] = expression.split(" ");
+  let result;
+  switch (operator) {
+    case "+":
+      result = +a + +b;
+      break;
+    case "-":
+      result = a - b;
+      break;
+    case "*":
+      result = a * b;
+      break;
+    case "/":
+      result = a / b;
+      break;
+  }
+  return `${expression} = ${result}`;
+};
+calc("1 + 2");
