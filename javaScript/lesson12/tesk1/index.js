0334a7ec428856e50048c3bd5f009407c4dd5cff
Ksondzyk <roman.kson@gmail.com>
2020-04-15 12:10:29 +0300

    calc

---
+++ javaScript/lesson12/tesk1/index.js
@@ -0,0 +1,20 @@
+const calc = (expression) => {
+  const [a, operator, b] = expression.split(" ");
+  let result;
+  switch (operator) {
+    case "+":
+      result = +a + +b;
+      break;
+    case "-":
+      result = a - b;
+      break;
+    case "*":
+      result = a * b;
+      break;
+    case "/":
+      result = a / b;
+      break;
+  }
+  return `${expression} = ${result}`;
+};
+console.log(calc("1 + 2"));
