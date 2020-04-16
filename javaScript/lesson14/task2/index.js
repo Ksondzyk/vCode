590a0f7ac7a981a3c5b732a79d63b219c7341467
Ksondzyk <roman.kson@gmail.com>
2020-04-16 20:39:00 +0300

    push

---
+++ javaScript/lesson14/task2/index.js
@@ -0,0 +1,33 @@
+let message = "Just learn it!";
+let sender = "Gromcode";
+const createMessenger = () => {
+  function sendMessage(name) {
+    console.log(`${name}, ${message} Your ${sender}`);
+  }
+
+  function setMessage(text) {
+    message = text;
+  }
+  function setSender(text) {
+    sender = text;
+  }
+
+  return {
+    sendMessage,
+    setMessage,
+    setSender,
+  };
+};
+
+const massager1 = createMessenger();
+
+massager1.sendMessage("Bob");
+
+const massager2 = createMessenger();
+massager2.setMessage("Good job!");
+massager2.sendMessage("Bob");
+
+const massager3 = createMessenger();
+massager3.setSender("Gromcode");
+massager3.setMessage("Good job!");
+massager3.sendMessage("Bob");
