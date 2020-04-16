7a6e585de09b41dc6aa2afbded12db62432bf3bd
Ksondzyk <roman.kson@gmail.com>
2020-04-16 15:45:15 +0300

    Lexical environment

---
+++ javaScript/lesson14/task1/index.js
@@ -0,0 +1,12 @@
+let message = "Just learn it!";
+const sendMessage = (name) => {
+  const sender = "Gromcode";
+  console.log(`${name}, ${message} Your ${sender}`);
+};
+
+function setMessage(text) {
+  message = text;
+}
+sendMessage("Ann");
+setMessage("Good job!");
+sendMessage("Ann");
