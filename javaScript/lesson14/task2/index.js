e103e9a1d62e424d712767162cf8a7166cc890a5
Ksondzyk <roman.kson@gmail.com>
2020-04-17 13:34:50 +0300

    reload

--- javaScript/lesson14/task2/index.js
+++ javaScript/lesson14/task2/index.js
@@ -1,8 +1,9 @@
 let message = "Just learn it!";
-let sender = "Gromcode";
+
 export default function createMessenger() {
+  let sender = "Gromcode";
   function sendMessage(name) {
-    console.log(`${name}, ${message} Your ${sender}`);
+    console.log(`${name}, ${message}! Your ${sender}`);
   }
 
   function setMessage(text) {
@@ -13,22 +14,9 @@ export default function createMessenger() {
     sender = text.bold();
   }
 
-  var obj = {
+  return {
     sendMessage: sendMessage,
     setMessage: setMessage,
     setSender: setSender,
   };
-  return obj;
 }
-
-// const massager1 = createMessenger();
-
-// massager1.sendMessage("Bob");
-
-// const massager2 = createMessenger();
-// massager2.setMessage("Good job!");
-
-// const massager3 = createMessenger();
-// massager3.setSender("Gromcode");
-// massager3.setMessage("Good job!");
-// massager3.sendMessage("Bob");
