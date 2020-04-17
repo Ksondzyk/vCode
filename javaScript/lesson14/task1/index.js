9ceab95c6ba263e43211f559b7c29102473feda2
Ksondzyk <roman.kson@gmail.com>
2020-04-17 13:18:57 +0300

    reload

--- javaScript/lesson14/task1/index.js
+++ javaScript/lesson14/task1/index.js
@@ -1,13 +1,13 @@
 "use strict";
-let message = "Just learn it!";
+let message = "Just learn it";
 export const sendMessage = (name) => {
   const sender = "Gromcode";
-  console.log(`${name}, ${message} Your ${sender}`);
+  console.log(`${name}, ${message}! Your ${sender}`);
 };
 
 export function setMessage(text) {
   message = text;
 }
 sendMessage("Ann");
-setMessage("Good job!");
+setMessage("Good job");
 sendMessage("Ann");
