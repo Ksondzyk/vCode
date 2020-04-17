68f4e2c3be12ef94b7c2c1086c736531b02a17c9
Ksondzyk <roman.kson@gmail.com>
2020-04-17 16:27:28 +0300

    rel

--- javaScript/lesson14/task2/createMessager.js
+++ javaScript/lesson14/task2/createMessager.js
@@ -1,22 +1,27 @@
 import createMessenger from "./index";
 
-const massager1 = createMessenger();
 
+
+const massager1 = createMessenger();
 massager1.sendMessage("Bob");
 
 const massager2 = createMessenger();
-massager2.setMessage("Good job!");
+massager2.setMessage("Good job");
+massager2.sendMessage("Bob");
 
 const massager3 = createMessenger();
 massager3.setSender("Gromcode");
+massager3.setMessage("Just learn it");
+massager3.sendMessage("Bob");
+
+
 // const massager1 = createMessenger();
 
 // massager1.sendMessage("Bob");
 
 // const massager2 = createMessenger();
-// massager2.setMessage("Good job");
+// massager2.setMessage("Good job!");
 
 // const massager3 = createMessenger();
 // massager3.setSender("Gromcode");
-// massager3.setMessage("Good job!");
-// massager3.sendMessage("Bob");
+
--- javaScript/lesson14/task2/index.js
+++ javaScript/lesson14/task2/index.js
@@ -1,11 +1,11 @@
 let message = "Just learn it";
-// let sender = "Gromcode";
+let sender = "Gromcode";
 export default function createMessenger() {
   let sender = "Gromcode";
   function sendMessage(name) {
+    // let sender = "Gromcode";
     console.log(`${name}, ${message}! Your ${sender}`);
   }
-
   function setMessage(text) {
     message = text;
   }
@@ -19,12 +19,17 @@ export default function createMessenger() {
     setSender: setSender,
   };
 }
-// const massager1 = createMessenger();
+const massager1 = createMessenger();
+massager1.sendMessage("Bob");
 
-// massager1.sendMessage("Bob");
+const massager2 = createMessenger();
+massager2.setMessage("Good job");
+massager2.sendMessage("Bob");
 
-// const massager2 = createMessenger();
-// massager2.setMessage("Good job");
+const massager3 = createMessenger();
+massager3.setSender("Gromcode");
+massager3.setMessage("Just learn it");
+massager3.sendMessage("Bob");
 
 // const massager3 = createMessenger();
 
