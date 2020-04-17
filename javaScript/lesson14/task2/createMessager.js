6422d9baff016ddf9937f1019126e0cf84ef4cbd
Ksondzyk <roman.kson@gmail.com>
2020-04-17 16:35:13 +0300

    rel

--- javaScript/lesson14/task2/createMessager.js
+++ javaScript/lesson14/task2/createMessager.js
@@ -1,7 +1,5 @@
 import createMessenger from "./index";
 
-
-
 const massager1 = createMessenger();
 massager1.sendMessage("Bob");
 
@@ -11,17 +9,16 @@ massager2.sendMessage("Bob");
 
 const massager3 = createMessenger();
 massager3.setSender("Gromcode");
-massager3.setMessage("Just learn it");
 massager3.sendMessage("Bob");
 
-
 // const massager1 = createMessenger();
-
 // massager1.sendMessage("Bob");
 
 // const massager2 = createMessenger();
-// massager2.setMessage("Good job!");
+// massager2.setMessage("Good job");
+// massager2.sendMessage("Bob");
 
 // const massager3 = createMessenger();
 // massager3.setSender("Gromcode");
-
+// massager3.setMessage("Just learn it");
+// massager3.sendMessage("Bob");
--- javaScript/lesson14/task2/index.js
+++ javaScript/lesson14/task2/index.js
@@ -1,7 +1,8 @@
-let message = "Just learn it";
-let sender = "Gromcode";
+// let message = "Just learn it";
+// let sender = "Gromcode";
 export default function createMessenger() {
-  // let sender = "Gromcode";
+  let message = "Just learn it";
+  let sender = "Gromcode";
   function sendMessage(name) {
     let sender = "Gromcode";
     console.log(`${name}, ${message}! Your ${sender}`);
@@ -19,17 +20,16 @@ export default function createMessenger() {
     setSender: setSender,
   };
 }
-const massager1 = createMessenger();
-massager1.sendMessage("Bob");
+// const massager1 = createMessenger();
+// massager1.sendMessage("Bob");
 
-const massager2 = createMessenger();
-massager2.setMessage("Good job");
-massager2.sendMessage("Bob");
+// const massager2 = createMessenger();
+// massager2.setMessage("Good job");
+// massager2.sendMessage("Bob");
 
-const massager3 = createMessenger();
-massager3.setSender("Gromcode");
-massager3.setMessage("Just learn it");
-massager3.sendMessage("Bob");
+// const massager3 = createMessenger();
+// massager3.setSender("Gromcode");
+// massager3.sendMessage("Bob");
 
 // const massager3 = createMessenger();
 
