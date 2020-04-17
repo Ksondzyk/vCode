ba3e300a08c49c16ed87b7d316c387639e65fac8
Ksondzyk <roman.kson@gmail.com>
2020-04-17 16:43:37 +0300

    reload

--- javaScript/lesson14/task2/createMessager.js
+++ javaScript/lesson14/task2/createMessager.js
@@ -5,10 +5,10 @@ massager1.sendMessage("Bob");
 
 const massager2 = createMessenger();
 massager2.setMessage("Good job");
-massager2.sendMessage("Bob");
+// massager2.sendMessage("Bob");
 
 const massager3 = createMessenger();
-massager3.sendMessage("Bob");
+// massager3.sendMessage("Bob");
 massager3.setSender("Gromcode");
 
 // const massager1 = createMessenger();
