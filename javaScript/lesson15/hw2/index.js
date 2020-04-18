c6712c0a1be5d33bb4296371b5db71a3c81cad96
Ksondzyk <roman.kson@gmail.com>
2020-04-19 00:06:35 +0300

    reload

--- javaScript/lesson15/hw2/index.js
+++ javaScript/lesson15/hw2/index.js
@@ -25,20 +25,20 @@ export function createLogger() {
       return [];
     }
     if (string === "warn" || string === "error" || string === "log") {
-      // let arrFilter = [];
-      // arrFilter = arr
-      //   .filter((el) => el.type === string)
-      //   .sort((a, b) => {
-      //     if (a.dateTime > b.dateTime) {
-      //       return -1;
-      //     }
-      //     if (a.dateTime < b.dateTime) {
-      //       return 1;
-      //     }
-      //   })
-      //   .map((el) => el.message);
-      // console.log(arrFilter);
-      // return arrFilter;
+      let arrFilter = [];
+      arrFilter = arr
+        .filter((el) => el.type === string)
+        .sort((a, b) => {
+          if (a.dateTime > b.dateTime) {
+            return -1;
+          }
+          if (a.dateTime < b.dateTime) {
+            return 1;
+          }
+        })
+        .map((el) => el.message);
+      console.log(arrFilter);
+      return arrFilter;
     } else if (string === undefined) {
       let arrSort = arr.sort((a, b) => {
         if (a.dateTime > b.dateTime) {
@@ -50,8 +50,6 @@ export function createLogger() {
       });
       //   console.log(arrSort);
       return arrSort;
-    } else if (typeof string === "string" || arr.length === 0) {
-      return [];
     }
   }
 
