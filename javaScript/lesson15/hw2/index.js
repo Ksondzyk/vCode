25be0e8c2876a014e7d115f28b495186502ba212
Ksondzyk <roman.kson@gmail.com>
2020-04-18 22:39:16 +0300

    reload

--- javaScript/lesson15/hw2/index.js
+++ javaScript/lesson15/hw2/index.js
@@ -1,36 +1,47 @@
-// let arr = [];
+let arr = [];
+
 export function createLogger() {
-  let arr = [];
+  var today = new Date();
+  today.setHours(today.getHours() + 4);
   let memory = {};
   function warn(str) {
     memory.message = str;
-    memory.dateTime = new Date();
+    memory.dateTime = today.setHours(today.getHours() + 2);
     memory.type = "warn";
     arr.push(memory);
   }
   function error(str) {
     memory.message = str;
-    memory.dateTime = new Date();
+    memory.dateTime = today.setHours(today.getHours() + 5);
     memory.type = "error";
     arr.push(memory);
   }
   function log(str) {
     memory.message = str;
-    memory.dateTime = new Date();
+    memory.dateTime = today.setHours(today.getHours() + 4);
     memory.type = "log";
     arr.push(memory);
   }
   function getRecords(string) {
     if (string === "warn" || string === "error" || string === "log") {
-      console.log(arr.filter((el) => el.type === string));
       arr.filter((el) => el === string);
     } else if (string === undefined) {
-      const arrSort = arr.sort((a, b) => b.dateTime - a.dateTime);
+      let arrSort = arr.sort((a, b) => b.dateTime - a.dateTime);
+
+      //   {
+      //     if (a.dateTime > b.dateTime) {
+      //       return 1;
+      //     }
+      //     if (a.dateTime < b.dateTime) {
+      //       return -1;
+      //     }
+      //   });
       console.log(arrSort);
+      return arrSort;
     } else if (typeof string == "string") {
       return [];
     }
-    return arr.sort((a, b) => b.dateTime - a.dateTime);
+    return arr;
   }
 
   return {
@@ -41,19 +52,16 @@ export function createLogger() {
   };
 }
 
-// const logger1 = createLogger();
-// logger1.warn("hello Warn");
+const logger1 = createLogger();
+logger1.warn("hello Warn");
 
-// const logger2 = createLogger();
-// logger2.error("hello Error");
-// const logger3 = createLogger();
-// logger3.log("hello Log");
+const logger2 = createLogger();
+logger2.error("hello Error");
+const logger3 = createLogger();
+logger3.log("hello Log");
 
-// // const logger3 = createLogger();
-// // logger3.log("hello Log");
-
-// // const logger4 = createLogger();
-// // logger4.getRecords("warn");
+const logger4 = createLogger();
+logger4.getRecords();
 
 // // const logger5 = createLogger();
 // // logger5.getRecords("warn");
