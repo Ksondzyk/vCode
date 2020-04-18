89430f30ac186f6e79cdaf1f0f647af1cd8090d7
Ksondzyk <roman.kson@gmail.com>
2020-04-18 17:11:08 +0300

    create logger

---
+++ javaScript/lesson15/hw2/index.js
@@ -0,0 +1,29 @@
+function createLogger() {
+  let memory = { message: "", dateTime: "17:44", type: "" };
+  function warn(str) {
+    memory.message = str;
+    memory.type = "warn";
+  }
+  function error(str) {
+    memory.message = str;
+    memory.type = "error";
+  }
+  function log(str) {
+    memory.message = str;
+    memory.type = "log";
+  }
+  return {
+    warn,
+    error,
+    log,
+  };
+}
+
+const logger1 = createLogger();
+logger1.warn("hello Warn");
+
+const logger2 = createLogger();
+logger2.error("hello Error");
+
+const logger3 = createLogger();
+logger3.log("hello Log");
