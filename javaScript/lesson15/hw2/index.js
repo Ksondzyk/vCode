5c91f2155a2afff236e9743dffd86dcadb590700
Ksondzyk <roman.kson@gmail.com>
2020-04-18 22:41:31 +0300

    reload

--- javaScript/lesson15/hw2/index.js
+++ javaScript/lesson15/hw2/index.js
@@ -52,16 +52,16 @@ export function createLogger() {
   };
 }
 
-const logger1 = createLogger();
-logger1.warn("hello Warn");
+// const logger1 = createLogger();
+// logger1.warn("hello Warn");
 
-const logger2 = createLogger();
-logger2.error("hello Error");
-const logger3 = createLogger();
-logger3.log("hello Log");
+// const logger2 = createLogger();
+// logger2.error("hello Error");
+// const logger3 = createLogger();
+// logger3.log("hello Log");
 
-const logger4 = createLogger();
-logger4.getRecords();
+// const logger4 = createLogger();
+// logger4.getRecords();
 
 // // const logger5 = createLogger();
 // // logger5.getRecords("warn");
