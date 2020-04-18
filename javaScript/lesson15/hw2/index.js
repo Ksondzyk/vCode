ce42c80d75a36cbe589f9008daab6b5f35bf0ead
Ksondzyk <roman.kson@gmail.com>
2020-04-18 18:57:13 +0300

    reload

--- javaScript/lesson15/hw2/index.js
+++ javaScript/lesson15/hw2/index.js
@@ -1,5 +1,5 @@
 export function createLogger() {
-  let memory = [];
+  let memory = {};
   function warn(str) {
     {
       memory.message = str;
@@ -39,20 +39,20 @@ export function createLogger() {
   };
 }
 
-// const logger1 = createLogger();
-// logger1.warn("hello Warn");
+const logger1 = createLogger();
+logger1.warn("hello Warn");
 
-// const logger2 = createLogger();
-// logger2.error("hello Error");
+const logger2 = createLogger();
+logger2.error("hello Error");
 
-// const logger3 = createLogger();
-// logger3.log("hello Log");
+const logger3 = createLogger();
+logger3.log("hello Log");
 
-// const logger4 = createLogger();
-// logger4.getRecords("warn");
+const logger4 = createLogger();
+logger4.getRecords("warn");
 
-// const logger5 = createLogger();
-// logger5.getRecords();
+const logger5 = createLogger();
+logger5.getRecords();
 
-// const logger6 = createLogger();
-// logger6.getRecords("red");
+const logger6 = createLogger();
+logger6.getRecords("red");
