483919821377ae918e39d54f2683dee19e8c23b9
Ksondzyk <roman.kson@gmail.com>
2020-04-18 22:44:33 +0300

    reloa

--- javaScript/lesson15/hw2/index.js
+++ javaScript/lesson15/hw2/index.js
@@ -1,24 +1,22 @@
 // let arr = [];
-
 export function createLogger() {
   let arr = [];
-  var today = new Date();
   let memory = {};
   function warn(str) {
     memory.message = str;
-    memory.dateTime = today.setHours(today.getHours() + 2);
+    memory.dateTime = "17:19";
     memory.type = "warn";
     arr.push(memory);
   }
   function error(str) {
     memory.message = str;
-    memory.dateTime = today.setHours(today.getHours() + 5);
+    memory.dateTime = "17:20";
     memory.type = "error";
     arr.push(memory);
   }
   function log(str) {
     memory.message = str;
-    memory.dateTime = today.setHours(today.getHours() + 4);
+    memory.dateTime = "17:18";
     memory.type = "log";
     arr.push(memory);
   }
@@ -26,16 +24,14 @@ export function createLogger() {
     if (string === "warn" || string === "error" || string === "log") {
       arr.filter((el) => el === string);
     } else if (string === undefined) {
-      let arrSort = arr.sort((a, b) => b.dateTime - a.dateTime);
-
-      //   {
-      //     if (a.dateTime > b.dateTime) {
-      //       return 1;
-      //     }
-      //     if (a.dateTime < b.dateTime) {
-      //       return -1;
-      //     }
-      //   });
+      let arrSort = arr.sort((a, b) => {
+        if (a.dateTime > b.dateTime) {
+          return 1;
+        }
+        if (a.dateTime < b.dateTime) {
+          return -1;
+        }
+      });
       console.log(arrSort);
       return arrSort;
     } else if (typeof string == "string") {
@@ -52,16 +48,16 @@ export function createLogger() {
   };
 }
 
-// const logger1 = createLogger();
-// logger1.warn("hello Warn");
+const logger1 = createLogger();
+logger1.warn("hello Warn");
 
-// const logger2 = createLogger();
-// logger2.error("hello Error");
-// const logger3 = createLogger();
-// logger3.log("hello Log");
+const logger2 = createLogger();
+logger2.error("hello Error");
+const logger3 = createLogger();
+logger3.log("hello Log");
 
-// const logger4 = createLogger();
-// logger4.getRecords();
+const logger4 = createLogger();
+logger4.getRecords();
 
 // // const logger5 = createLogger();
 // // logger5.getRecords("warn");
