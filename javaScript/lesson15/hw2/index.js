a257f88d69358feb4fe936d34484e79a31d929c4
Ksondzyk <roman.kson@gmail.com>
2020-04-20 17:56:03 +0300

    reload

--- javaScript/lesson15/hw2/index.js
+++ javaScript/lesson15/hw2/index.js
@@ -28,7 +28,7 @@ export function createLogger() {
     }
     if (type === "warn" || type === "error" || type === "log") {
       let arrSort = arr.slice().sort((a, b) => {
-        return b.dateTime - a.dateTime;
+        return a.dateTime - b.dateTime;
       });
       // let arrSort = arr.slice().sort((a, b) => {
       //   if (a.dateTime > b.dateTime) {
