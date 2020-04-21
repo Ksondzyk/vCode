3fa7265eaf80c845028263b9781ed32c4450c111
Ksondzyk <roman.kson@gmail.com>
2020-04-21 16:15:21 +0300

    reload

--- javaScript/lesson16/hw1/index.js
+++ javaScript/lesson16/hw1/index.js
@@ -1,14 +1,14 @@
 //input  number
 //return arr [functions ]
 export function createArrayOfFunctions(number) {
-  if (typeof number !== Number()) {
-    return null;
-  }
+  //   if (typeof number !== typeof Number) {
+  //     return null;
+  //   }
   if (number === undefined) {
     return [];
   }
   let arr = [];
-  for (let i = 0; i < number; i++) {
+  for (let i = 0; i < number.length; i++) {
     arr.push(
       (arr[i] = function () {
         return i;
