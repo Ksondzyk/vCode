e75978b9c6cfd3bd8dcac491f58321728475025c
Ksondzyk <roman.kson@gmail.com>
2020-04-22 12:17:06 +0300

    reload

--- javaScript/lesson17/task1/index.js
+++ javaScript/lesson17/task1/index.js
@@ -1,6 +1,9 @@
-export const user = { firstName: "John", lastName: "Doe" };
-
-user.getFullName = function () {
-  console.log(`${this.firstName} ${this.lastName}`);
+export const user = {
+  firstName: "John",
+  lastName: "Doe",
+  getFullName: function () {
+    console.log(`${this.firstName} ${this.lastName}`);
+  },
 };
+
 user.getFullName();
