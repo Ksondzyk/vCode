74bd8b5b9ca967f777e4e6daff1ec89a87ce0096
Ksondzyk <roman.kson@gmail.com>
2020-04-22 14:09:06 +0300

    reload

--- javaScript/lesson17/task3/index.js
+++ javaScript/lesson17/task3/index.js
@@ -1,8 +1,8 @@
-export function defer(func, ms) {
+export const defer = (func, ms) => {
   return function () {
     setTimeout(func, ms);
   };
-}
+};
 
 const sayHi = () => {
   console.log("Hi");
