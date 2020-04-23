326fb3404f9e293983c8056a2f5031df83910e00
Ksondzyk <roman.kson@gmail.com>
2020-04-23 18:45:41 +0300

    push

--- javaScript/lesson18/task3/index.js
+++ javaScript/lesson18/task3/index.js
@@ -1,5 +1,7 @@
 export function sumOfSquares() {
-  return [...arguments].map((elem) => elem * elem);
+  return [...arguments]
+    .map((elem) => elem * elem)
+    .reduce((acc, elem) => (acc += elem));
 }
 
 console.log(sumOfSquares(1, 2, 3, 4));
