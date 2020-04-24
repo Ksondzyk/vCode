0a0d67c8992eb2f67c913d9cc87e2ce29c89eb88
Ksondzyk <roman.kson@gmail.com>
2020-04-24 13:23:01 +0300

    reload

--- javaScript/lesson18/hw1/index.js
+++ javaScript/lesson18/hw1/index.js
@@ -1,12 +1,9 @@
-export function saveCalls() {
+export function saveCalls(func) {
   const calls = [];
-
   function withMemory(...args) {
-    calls.push(...args);
+    calls.push(args);
     return func.apply(this, args);
   }
   withMemory.calls = calls;
   return withMemory;
 }
-
-
