21ac62cbb3f38365c225e46da487c1d5918e771d
Ksondzyk <roman.kson@gmail.com>
2020-04-24 23:51:27 +0300

    push

--- javaScript/lesson19/task2/index.js
+++ javaScript/lesson19/task2/index.js
@@ -12,37 +12,42 @@ const vehicle = {
 
 // console.log(vehicle.move());
 
-const ship = {
-  name: "Argo",
-  hasWheels: false,
-  startMachine() {
-    console.log(`${this.name} lifting anchor up`);
-    this.move();
-  },
-  age: 12,
-  stopMachine() {
-    console.log(`${this.name} lifting anchor down`);
-    this.stop();
-  },
-};
-Object.setPrototypeOf(ship, vehicle);
+// const ship = {
+//   name: "Argo",
+//   hasWheels: false,
+//   startMachine() {
+//     console.log(`${this.name} lifting anchor up`);
+//     this.move();
+//   },
+//   age: 12,
+//   stopMachine() {
+//     console.log(`${this.name} lifting anchor down`);
+//     this.stop();
+//   },
+// };
+// Object.setPrototypeOf(ship, vehicle);
 
-// console.log(typeof ship.stopMachine());
-// for (let prop in ship) {
-//   if (ship.hasOwnProperty(prop)) {
-//     console.log(prop);
+// export function getOwnProps(obj) {
+//   console.log(obj);
+//   const arr = [];
+//   for (let prop in obj) {
+//     if (!(obj[prop] instanceof Function) && ship.hasOwnProperty(prop)) {
+//       arr.push(prop);
+//     }
 //   }
+//   return arr;
 // }
+// console.log(getOwnProps(ship));
 
 export function getOwnProps(obj) {
-  console.log(obj);
-  const arr = [];
-  for (let prop in obj) {
-    if (!(obj[prop] instanceof Function) && ship.hasOwnProperty(prop)) {
-      arr.push(prop);
+  const arr = Object.entries(obj).flat();
+  const ownProps = arr.reduce((acc, el, index) => {
+    if (!(el instanceof Function) && index % 2 === 1) {
+      acc.push(arr[index - 1]);
     }
-  }
-  return arr;
+    return acc;
+  }, []);
+  return ownProps;
 }
 console.log(getOwnProps(ship));
 
