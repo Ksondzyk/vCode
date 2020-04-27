e607b9eb3b5fb2d862cd62adabb1162aa528ef3b
Ksondzyk <roman.kson@gmail.com>
2020-04-27 13:55:33 +0300

    class User

---
+++ javaScript/lesson20/task1/index.js
@@ -0,0 +1,31 @@
+export class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  sayHi() {
+    console.log(`Hi, I am ${this.name}`);
+  }
+  requestNewPhoto() {
+    console.log(`New photo request was sent for ${this.name}`);
+  }
+  setAge(value) {
+    if (value < 0) {
+      return false;
+    }
+    this.age = value;
+    if (age >= 25) {
+      this.requestNewPhoto();
+    }
+    return value;
+  }
+}
+
+const user1 = new User("Tom", 25);
+
+console.log(user1);
+
+// user1.sayHi();
+// user2.requestNewPhoto();
+// user3.setAge(12);
+// user4.setAge(25);
