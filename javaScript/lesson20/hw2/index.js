2e92ac549e3b603ba71c96303740451eef88bd61
Ksondzyk <roman.kson@gmail.com>
2020-04-27 23:17:02 +0300

    hw2

---
+++ javaScript/lesson20/hw2/index.js
@@ -0,0 +1,39 @@
+class User {
+  id = "";
+  name = "";
+  sessionId = "";
+  constructor(id, name, sessionId) {
+    this.id = id;
+    this.name = name;
+    this.sessionId = sessionId;
+  }
+  get id() {
+    return this.id;
+  }
+  get name() {
+    return this.name;
+  }
+  get sessionId() {
+    return this.sessionId;
+  }
+}
+
+class UserRepository {
+  arrUser = [];
+  constructor(users) {
+    this.users = users;
+    for (let i = 0; i < users.length; i++) {
+      Object.freeze(users[i]);
+    }
+  }
+
+  getUserNames() {
+    return this.users.map((user) => user.name());
+  }
+  getUserIds() {
+    return this.users.map((user) => user.id());
+  }
+  getUserNameById(id) {
+    return this.users.find((id) => id === id).name();
+  }
+}
