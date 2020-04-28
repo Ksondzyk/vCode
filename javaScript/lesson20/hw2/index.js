a5e3668c93645fa71db2b4fe4924cab182588fa3
Ksondzyk <roman.kson@gmail.com>
2020-04-28 15:36:16 +0300

    push

--- javaScript/lesson20/hw2/index.js
+++ javaScript/lesson20/hw2/index.js
@@ -1,7 +1,4 @@
 export class User {
-  _id = "";
-  _name = "";
-  _sessionId = "";
   constructor(id, name, sessionId) {
     this._id = id;
     this._name = name;
@@ -17,28 +14,21 @@ export class User {
     return this._sessionId;
   }
 }
-
 export class UserRepository {
   constructor(users) {
-    this._users = users;
-    for (let i = 0; i < users; i++) {
-      Object.freeze(users[i]);
-    }
+    this._users = Object.freeze(users);
   }
-  // get users() {
-  //   return this._users;
-  // }
+  get users() {
+    return this._users;
+  }
   getUserNames() {
-    return this.users.map((user) => user.name);
+    return this._users.map(({ name }) => name);
   }
   getUserIds() {
-    return this.users.map((user) => user.id);
+    return this._users.map(({ id }) => id);
   }
   getUserNameById(id) {
-    return this.users.find((user) => id === user.id).name;
+    const user = this._users.find((user) => user.id === id);
+    return user ? user.name : null;
   }
 }
-// const user1 = new User(12, "roman", 12);
-
-// const user2 = new UserRepository(user1);
-// console.log(user2._users);
