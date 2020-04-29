52cb9444d3f2e5757a8a26ea7457b06b6e9cce50
Ksondzyk <roman.kson@gmail.com>
2020-04-29 11:22:32 +0300

    dom task1

---
+++ javaScript/lesson21/hw1/index.js
@@ -0,0 +1,11 @@
+export const titleElement = document.querySelector(".title");
+function getTitleElement(elem) {
+  console.dir(elem);
+}
+
+getTitleElement(titleElement);
+const inputText = document.querySelector("input[type=text]").value;
+export function getInputElement(inputElem) {
+  console.dir(inputElem);
+}
+getInputElement(inputText);
