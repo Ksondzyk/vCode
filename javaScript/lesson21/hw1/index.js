64fcf7dcd4717ca59ba82899476ac5eb43185757
Ksondzyk <roman.kson@gmail.com>
2020-04-29 20:11:43 +0300

    push

--- javaScript/lesson21/hw1/index.js
+++ javaScript/lesson21/hw1/index.js
@@ -1,12 +1,13 @@
 export function getTitleElement() {
   const titleElem = document.querySelector(".title");
   console.dir(titleElem);
+  return titleElem;
 }
 // getTitleElement();
 
 export function getInputElement() {
-  const inputElem = document.querySelector("input");
-  const typeText = inputElem.querySelector("value");
-  console.dir(typeText);
+  const inputElem = document.querySelector("input[type=text]");
+  console.dir(inputElem);
+  return inputElem;
 }
 // getInputElement();
