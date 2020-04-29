d78f45fdb13397f986a69e135bdf7d09658ff27c
Ksondzyk <roman.kson@gmail.com>
2020-04-29 15:45:47 +0300

    push

--- javaScript/lesson21/hw7/index.js
+++ javaScript/lesson21/hw7/index.js
@@ -1,6 +1,7 @@
 export function createButton(buttonText) {
   const body = document.querySelector("body");
-  const createElem = (document.createElement("button").innerText = buttonText);
-  return body.append(createElem);
+  const createElem = document.createElement("button");
+  createElem.textContent = buttonText;
+  body.append(createElem);
 }
-// createButton("sund Email");
+createButton("sund Email");
