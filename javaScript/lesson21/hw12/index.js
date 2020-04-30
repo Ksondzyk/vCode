639bc4e0206fef25b2399ee7736d59114540ea0f
Ksondzyk <roman.kson@gmail.com>
2020-04-30 13:49:19 +0300

    push

--- javaScript/lesson21/hw12/index.js
+++ javaScript/lesson21/hw12/index.js
@@ -1,14 +1,5 @@
 export function getSection(num) {
-  const parent = document.querySelectorAll(".box");
-  const element = document.querySelectorAll(`span[data-number="${num}"]`);
-  let res = "";
-  for (let i = 0; i < parent.length; i++) {
-    const element = parent[i].querySelectorAll(`span[data-number="${num}"]`);
-    if (element) {
-      res = parent[i].dataset.section;
-      break;
-    }
-  }
-  return res;
+  const element = document.querySelector(`span[data-number="${num}"]`);
+  return element.closest(".box");
 }
-// getSection(2);
+getSection(6);
