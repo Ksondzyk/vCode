323acd67a10ec06abb3ad382e00a5b2817f25843
Ksondzyk <roman.kson@gmail.com>
2020-05-01 22:39:02 +0300

    push

---
+++ javaScript/lesson22/hw9/index.js
@@ -0,0 +1,8 @@
+const btn1 = document.querySelectorAll(".pagination__page");
+
+function handleClick(event) {
+  console.log(event.target.textContent);
+}
+btn1[0].addEventListener("click", handleClick);
+btn1[1].addEventListener("click", handleClick);
+btn1[2].addEventListener("click", handleClick);
