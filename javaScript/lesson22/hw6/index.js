c7895507da5589aacd5b7da43bb42abcd9c95ab4
Ksondzyk <roman.kson@gmail.com>
2020-05-01 22:07:33 +0300

    push

--- javaScript/lesson22/hw6/index.js
+++ javaScript/lesson22/hw6/index.js
@@ -3,9 +3,5 @@ const btn1 = document.querySelectorAll(".btn");
 function handleClick(event) {
   console.log(event.target.textContent);
 }
-btn1[0].addEventListener("click", function click(event) {
-  handleClick(event);
-});
-btn1[1].addEventListener("click", function click(event) {
-  handleClick(event);
-});
+btn1[0].addEventListener("click", handleClick);
+btn1[1].addEventListener("click", handleClick);
