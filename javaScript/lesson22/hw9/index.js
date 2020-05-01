bc7ff9377d53ef249b18021e35760d7ba27fab31
Ksondzyk <roman.kson@gmail.com>
2020-05-01 22:43:22 +0300

    push

--- javaScript/lesson22/hw9/index.js
+++ javaScript/lesson22/hw9/index.js
@@ -1,7 +1,7 @@
 const btn1 = document.querySelectorAll(".pagination__page");
 
 function handleClick(event) {
-  console.log(event.target.textContent);
+  console.log(event.target.dataset.pageNumber);
 }
 btn1[0].addEventListener("click", handleClick);
 btn1[1].addEventListener("click", handleClick);
