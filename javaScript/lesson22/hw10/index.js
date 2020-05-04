d472418dbfd09ecaaed0cf161a93dac0ed3494ea
Ksondzyk <roman.kson@gmail.com>
2020-05-04 15:53:28 +0300

    push

--- javaScript/lesson22/hw10/index.js
+++ javaScript/lesson22/hw10/index.js
@@ -89,23 +89,23 @@ const renderListItem = (listItems) => {
 };
 renderListItem(tasks);
 
-// const btnCreate = document.querySelector(".create-task-btn");
+const btnCreate = document.querySelector(".create-task-btn");
 
-// btnCreate.addEventListener("click", createTask);
+btnCreate.addEventListener("click", createTask);
 
-// function createTask() {
-//   const input = document.querySelector(".task-input");
-//   if (input.value.length === 0) {
-//     return;
-//   }
-//   const newTask = [{}].map((text, done) => ({
-//     text: input.value,
-//     done: false,
-//   }));
-//   renderListItem(newTask);
-//   input.value = null;
-//   console.log(newTask);
-//   return newTask;
-// }
-// console.log(newList);
-// console.log(tasks);
+function createTask() {
+  const input = document.querySelector(".task-input");
+  if (input.value.length === 0) {
+    return;
+  }
+  const newTask = [{}].map((text, done) => ({
+    text: input.value,
+    done: false,
+  }));
+  renderListItem(newTask);
+  input.value = null;
+  console.log(newTask);
+  return newTask;
+}
+console.log(newList);
+console.log(tasks);
