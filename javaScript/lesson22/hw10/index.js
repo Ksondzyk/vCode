95d7f8a267f9882677e3cd90907762f1e7d706ac
Ksondzyk <roman.kson@gmail.com>
2020-05-05 12:50:02 +0300

    push

--- javaScript/lesson22/hw10/index.js
+++ javaScript/lesson22/hw10/index.js
@@ -64,53 +64,53 @@ renderListItem(tasks);
 // }
 // updateCheck();
 
-const tasks = [
-  { text: "Buy milk", done: false },
-  { text: "Pick up Tom from airport", done: false },
-  { text: "Visit party", done: false },
-  { text: "Visit doctor", done: true },
-  { text: "Buy meat", done: true },
-];
+// const tasks = [
+//   { text: "Buy milk", done: false },
+//   { text: "Pick up Tom from airport", done: false },
+//   { text: "Visit party", done: false },
+//   { text: "Visit doctor", done: true },
+//   { text: "Buy meat", done: true },
+// ];
 
-const listElem = document.querySelector(".list");
-renderListItem(tasks);
-function addTask(text, checked) {
-  tasks.push({ text: text, done: false });
-  renderListItem(tasks);
-}
+// const listElem = document.querySelector(".list");
+// renderListItem(tasks);
+// function addTask(text, checked) {
+//   tasks.push({ text: text, done: false });
+//   renderListItem(tasks);
+// }
 
-function addNewItem(text, checked) {
-  const el = document.createElement("li");
-  el.classList.add("list__item");
-  if (checked) {
-    el.classList.add("list__item_done");
-  }
-  const checkElem = document.createElement("input");
-  checkElem.setAttribute("type", "checkbox");
-  checkElem.checked = checked;
+// function addNewItem(text, checked) {
+//   const el = document.createElement("li");
+//   el.classList.add("list__item");
+//   if (checked) {
+//     el.classList.add("list__item_done");
+//   }
+//   const checkElem = document.createElement("input");
+//   checkElem.setAttribute("type", "checkbox");
+//   checkElem.checked = checked;
 
-  checkElem.classList.add("list__item-checkbox");
-  el.append(checkElem, text);
-  el.addEventListener("click", () => {
-    const checkedValues = el.firstChild;
-    if (checkedValues.checked) {
-      el.classList.add("list__item_done");
-    } else {
-      el.classList.remove("list__item_done");
-    }
-  });
-  listElem.append(el);
-}
-document.querySelector(".create-task-btn").addEventListener("click", () => {
-  let inputValue = document.querySelector(".task-input").value;
-  addTask(inputValue, false);
-  document.querySelector(".task-input").value = "";
-});
+//   checkElem.classList.add("list__item-checkbox");
+//   el.append(checkElem, text);
+//   el.addEventListener("click", () => {
+//     const checkedValues = el.firstChild;
+//     if (checkedValues.checked) {
+//       el.classList.add("list__item_done");
+//     } else {
+//       el.classList.remove("list__item_done");
+//     }
+//   });
+//   listElem.append(el);
+// }
+// document.querySelector(".create-task-btn").addEventListener("click", () => {
+//   let inputValue = document.querySelector(".task-input").value;
+//   addTask(inputValue, false);
+//   document.querySelector(".task-input").value = "";
+// });
 
-function renderListItem(listItems) {
-  document.querySelector(".list").classList.remove("list-item");
-  listItems = listItems.sort((a, b) => b.done - a.done);
-  listItems.forEach((item) => {
-    addNewItem(item.text, item.done);
-  });
-}
+// function renderListItem(listItems) {
+//   document.querySelector(".list").classList.remove("list-item");
+//   listItems = listItems.sort((a, b) => b.done - a.done);
+//   listItems.forEach((item) => {
+//     addNewItem(item.text, item.done);
+//   });
+// }
