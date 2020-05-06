8f7a936689c77b5e32302386d7727d28a5b57da7
Ksondzyk <roman.kson@gmail.com>
2020-05-06 20:08:20 +0300

    push

--- javaScript/lesson24/task1/import.js
+++ javaScript/lesson24/task1/import.js
@@ -1,1 +1,2 @@
 import { dayOfWeek } from "index.js";
+dayOfWeek(new Date(2020, 0, 1), 14);
--- javaScript/lesson24/task1/index.js
+++ javaScript/lesson24/task1/index.js
@@ -11,6 +11,6 @@ export const dayOfWeek = (date, days) => {
   const dateInFuture = new Date(date).setDate(day + days);
   return weekDay[new Date(dateInFuture).getDay()];
 };
-// const result = dayOfWeek(new Date(2020, 0, 1), 14);
+const result = dayOfWeek(new Date(2020, 0, 1), 14);
 
-// console.log(result);
+console.log(result);
