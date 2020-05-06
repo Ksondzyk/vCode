1d86dea12c3fadc17b5fa197e29ec4af732634b4
Ksondzyk <roman.kson@gmail.com>
2020-05-06 20:04:58 +0300

    date

---
+++ javaScript/lesson24/task1/index.js
@@ -0,0 +1,15 @@
+//input date, days
+//output string
+
+// узнать какой день по порядку в дате
+// новой дате прибавил старое колисчество дней + новое
+const weekDay = ["Sun", "Mon", "Tue", "Wed", "Fri", "Set"];
+
+export const dayOfWeek = (date, days) => {
+  const day = new Date(date).getDate();
+  const dateInFuture = new Date(date).setDate(day + days);
+  return weekDay[new Date(dateInFuture).getDay()];
+};
+// const result = dayOfWeek(new Date(2020, 0, 1), 14);
+
+// console.log(result);
