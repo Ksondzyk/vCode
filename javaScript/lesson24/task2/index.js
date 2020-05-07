8c4619ad3c9ed3f0955e88bcb54c75473e1584ba
Ksondzyk <roman.kson@gmail.com>
2020-05-07 14:39:05 +0300

    push

---
+++ javaScript/lesson24/task2/index.js
@@ -0,0 +1,10 @@
+//
+const formater = new Intl.DateTimeFormat("en", {
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: false,
+});
+
+export const getGreenwichTime = (date) => formater.format(date);
+
+// console.log(getGreenwichTime(new Date()));
