049237dd9295f3abd445b5eef33e40228be809b3
Ksondzyk <roman.kson@gmail.com>
2020-05-12 18:00:12 +0300

    push

--- javaScript/lesson27/task1/index.js
+++ javaScript/lesson27/task1/index.js
@@ -1,22 +1,18 @@
-// localStorage.clear();
-// localStorage.setItem("hobbies", JSON.stringify({ name: "Tom" }));
-// localStorage.setItem("name", "Tom");
-// localStorage.setItem("name", JSON.stringify(17));
-
-// console.log(JSON.parse(localStorage.getItem("hobbies")));
+localStorage.setItem("names", JSON.stringify({ name: "Tom" }));
+localStorage.setItem("name", "Tom");
 
 export const getLocalStorageData = () => {
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
-    // let newValue;
-    // try {
-    //   newValue = JSON.parse(value);
-    // } catch (e) {
-    //   newValue = value;
-    // }
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
     return {
       ...acc,
-      [key]: JSON.parse(value),
+      [key]: newValue,
     };
   }, {});
 };
-console.log(typeof getLocalStorageData());
+console.log(getLocalStorageData());
