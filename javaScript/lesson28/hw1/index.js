4c8005290902dc7965d7bb80c863cbb0a46d18dc
Ksondzyk <roman.kson@gmail.com>
2020-05-14 22:57:37 +0300

    push

---
+++ javaScript/lesson28/hw1/index.js
@@ -0,0 +1,65 @@
+//input
+//output obj in metods
+
+const shmoment = (objDate) => {
+  let result = new Date(objDate);
+  const calculate = {
+    add(data, value) {
+      switch (data) {
+        case "months":
+          result.setMinutes(value + result.getMonth());
+          break;
+        case "days":
+          result.setMinutes(value + result.getDay());
+          break;
+        case "hours":
+          result.setMinutes(value + result.getHours());
+          break;
+        case "minutes":
+          result.setMinutes(value + result.getMinutes());
+          break;
+        case "seconds":
+          result.setMinutes(value + result.getSeconds());
+          break;
+        case "miliseconds":
+          result.setMinutes(value + result.getMilliseconds());
+          break;
+      }
+      return this;
+    },
+    subtract(data, value) {
+      switch (data) {
+        case "months":
+          result.setMinutes(value - result.getMonth());
+          break;
+        case "days":
+          result.setMinutes(value - result.getDay());
+          break;
+        case "hours":
+          result.setMinutes(value - result.getHours());
+          break;
+        case "minutes":
+          result.setMinutes(value - result.getMinutes());
+          break;
+        case "seconds":
+          result.setMinutes(value - result.getSeconds());
+          break;
+        case "miliseconds":
+          result.setMinutes(value - result.getMilliseconds());
+          break;
+      }
+      return this;
+    },
+    result() {
+      return result;
+    },
+  };
+  return calculate;
+};
+const result = shmoment(new Date(2020, 0, 7, 17, 17, 17))
+  .add("minutes", 2)
+  .add("days", 8)
+  .subtract("years", 1)
+  .result(); // ... Jan 15 2019 17:19:17 ...
+
+console.log(result);
