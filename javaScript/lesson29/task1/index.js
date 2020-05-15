6ba2ef75ce50720c3b4474140ba87bf5c0b243ec
Ksondzyk <roman.kson@gmail.com>
2020-05-15 21:56:23 +0300

    push

--- javaScript/lesson29/task1/index.js
+++ javaScript/lesson29/task1/index.js
@@ -47,13 +47,13 @@ export const addImage = (imgSrc, callback) => {
 const imgSrc =
   "https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson+13/task2/sprites.png";
 
-const onImageLoaded = (error, data) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const { width, height } = data;
-  const sizeElem = document.querySelector(".image-size");
-  sizeElem.textContent = `${width} x ${height}`;
-};
-addImage(imgSrc, onImageLoaded);
+// const onImageLoaded = (error, data) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+//   const { width, height } = data;
+//   const sizeElem = document.querySelector(".image-size");
+//   sizeElem.textContent = `${width} x ${height}`;
+// };
+// addImage(imgSrc, onImageLoaded);
