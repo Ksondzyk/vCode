ff3205c0329d127ddb426356be0dc422f5017191
Ksondzyk <roman.kson@gmail.com>
2020-05-18 18:32:03 +0300

    push

---
+++ javaScript/lesson29/hw1/index.js
@@ -0,0 +1,8 @@
+const callback = setInterval(() => {
+  const obj = { userId: "userid777", email: "userid777@example.com" };
+}, 2000);
+export function requestUserData(userId, callback) {
+  if (userId === typeof "broken") {
+  }
+}
+requestUserData("broken", callback);
