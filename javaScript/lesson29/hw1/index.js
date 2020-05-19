3cb8900b6fcaaefff17e274221aadf5835838e38
Ksondzyk <roman.kson@gmail.com>
2020-05-19 12:57:43 +0300

    push

--- javaScript/lesson29/hw1/index.js
+++ javaScript/lesson29/hw1/index.js
@@ -3,11 +3,9 @@ const max = 3000;
 let random = parseInt(Math.random() * (+max - +min) + +min);
 
 export function requestUserData(userId, callback) {
-  if (userId === "broken") {
-    const emty = () => {
-      return "Failed to load user data";
-    };
-    emty();
+  if (userId == "broken") {
+    callback(null, "Failed to load user data");
+    return;
   }
   setInterval(() => {
     const obj = { userId: `${userId}`, email: `${userId}@example.com` };
