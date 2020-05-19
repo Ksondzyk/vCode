4fe7fc63d9c263132a6f789ca01050b845a459ae
Ksondzyk <roman.kson@gmail.com>
2020-05-19 12:46:32 +0300

    push

--- javaScript/lesson29/hw1/index.js
+++ javaScript/lesson29/hw1/index.js
@@ -4,10 +4,12 @@ let random = parseInt(Math.random() * (+max - +min) + +min);
 
 export function requestUserData(userId, callback) {
   if (userId === "broken") {
+    () => {
+      return "Failed to load user data";
+    };
   }
   setInterval(() => {
     const obj = { userId: `${userId}`, email: `${userId}@example.com` };
-    // console.log(obj);
   }, callback);
 }
-requestUserData("userid777", random);
+requestUserData("broken", random);
