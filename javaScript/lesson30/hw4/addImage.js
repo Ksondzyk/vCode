d10fb40e1ce3cca8813d554df0711dab3424bc92
Ksondzyk <roman.kson@gmail.com>
2020-05-19 23:41:18 +0300

    push

--- javaScript/lesson30/hw4/addImage.js
+++ javaScript/lesson30/hw4/addImage.js
@@ -1,3 +1,5 @@
+import { addImage } from "./index.js";
+
 export const addImageV2 = (imageSrc) => {
   const elemPage = document.querySelector(".page");
   elemPage.setAttribute("src", imageSrc);
@@ -7,33 +9,8 @@ export const addImageV2 = (imageSrc) => {
       if (src) return resolve(src);
     });
   });
-
-  // // .then((onImageLoaded) => onImageLoaded.onImageLoaded())
-  // .then((img) => console.log(img))
 };
-function func(obj) {
-  console.log(obj);
-}
-export const addImage = (url, callback) => {
-  const img = document.createElement("img");
-  img.setAttribute("alt", "User avatar");
-  img.src = url;
 
-  const pageElem = document.querySelector(".page");
-  pageElem.append(img);
-
-  const onImageLoaded = () => {
-    const { width, height } = img;
-    callback(null, { width, height });
-    // return img;
-  };
-
-  const onImageLoadError = () => callback("Image load failed");
-
-  img.addEventListener("load", onImageLoaded);
-
-  img.addEventListener("error", onImageLoadError);
-};
 const imgSrc =
   "https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson+13/task2/sprites.png";
 
