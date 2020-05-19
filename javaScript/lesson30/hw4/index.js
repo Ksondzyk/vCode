a697e6235d4099a6fc38937e74bbe4e26fdf81ad
Ksondzyk <roman.kson@gmail.com>
2020-05-19 23:44:08 +0300

    push

--- javaScript/lesson30/hw4/index.js
+++ javaScript/lesson30/hw4/index.js
@@ -1,20 +1,10 @@
-export const addImage = (url, callback) => {
-  const img = document.createElement("img");
-  img.setAttribute("alt", "User avatar");
-  img.src = url;
-
-  const pageElem = document.querySelector(".page");
-  pageElem.append(img);
-
-  const onImageLoaded = () => {
-    const { width, height } = img;
-    callback(null, { width, height });
-    // return img;
-  };
-
-  const onImageLoadError = () => callback("Image load failed");
-
-  img.addEventListener("load", onImageLoaded);
-
-  img.addEventListener("error", onImageLoadError);
+export const addImageV2 = (imageSrc) => {
+  const elemPage = document.querySelector(".page");
+  elemPage.setAttribute("src", imageSrc);
+  return new Promise((resolve, reject) => {
+    return addImage(imageSrc, function (error, src) {
+      if (error) return reject(error);
+      if (src) return resolve(src);
+    });
+  });
 };
