bff4d5e00a5ab2c827eccae32d871951d94b8cf7
Ksondzyk <roman.kson@gmail.com>
2020-05-19 22:57:30 +0300

    push

--- javaScript/lesson30/hw4/addImage.js
+++ javaScript/lesson30/hw4/addImage.js
@@ -1,27 +1,28 @@
 export const addImageV2 = (src) => {
   return new Promise((resolve, reject) => {
-    const addImage = (url, callback) => {
-      const img = document.createElement("img");
-      img.setAttribute("alt", "User avatar");
-      img.src = url;
-
-      const pageElem = document.querySelector(".page");
-      pageElem.append(img);
-
-      const onImageLoaded = () => {
-        const { width, height } = img;
-        callback(null, { width, height });
-        return img;
-      };
-
-      const onImageLoadError = () => callback("Image load failed");
-
-      img.addEventListener("load", onImageLoaded);
-
-      img.addEventListener("error", onImageLoadError);
-    };
     return addImage;
   });
+};
+
+export const addImage = (url, callback) => {
+  const img = document.createElement("img");
+  img.setAttribute("alt", "User avatar");
+  img.src = url;
+
+  const pageElem = document.querySelector(".page");
+  pageElem.append(img);
+
+  const onImageLoaded = () => {
+    const { width, height } = img;
+    callback(null, { width, height });
+    return img;
+  };
+
+  const onImageLoadError = () => callback("Image load failed");
+
+  img.addEventListener("load", onImageLoaded);
+
+  img.addEventListener("error", onImageLoadError);
 };
 const imgSrc =
   "https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson+13/task2/sprites.png";
