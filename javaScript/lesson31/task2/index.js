fec9284caa185c383e21c6d9dc75ba728906a92e
Ksondzyk <roman.kson@gmail.com>
2020-05-20 18:40:01 +0300

    push

--- javaScript/lesson31/task2/index.js
+++ javaScript/lesson31/task2/index.js
@@ -1,7 +1,7 @@
 export const asyncCalculator = (num) =>
   new Promise((resolve) => {
     setTimeout(() => {
-      console.log(`Initial Value: ${num}`);
+      console.log(`Initial value: ${num}`);
       resolve(num);
     }, 500);
   })
@@ -10,14 +10,14 @@ export const asyncCalculator = (num) =>
         new Promise((resolve) => {
           setTimeout(() => {
             const result = value * value;
-            console.log(`square Value: ${result}`);
+            console.log(`Squared value: ${result}`);
             resolve(result);
           }, 500);
         })
     )
     .then((value) => {
       const result = value * 2;
-      console.log(`Doubled Value: ${result}`);
+      console.log(`'Doubled value: ${result}`);
       return result;
     });
 asyncCalculator(4).then((value) => console.log(value));
