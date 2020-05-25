06e06bcb5b890052ae9bb2de1f1c52e14fcf277c
Ksondzyk <roman.kson@gmail.com>
2020-05-25 19:17:54 +0300

    push

--- javaScript/lesson33/hw1/index.js
+++ javaScript/lesson33/hw1/index.js
@@ -1,7 +1,8 @@
 const baseUrl = "https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks";
 
-export const getTasksList = (baseUrl) =>
-  fetch(`${baseUrl}`).then((response) => response.json());
+export const getTasksList = (baseUrl) => {
+  return fetch(`${baseUrl}`).then((response) => response.json());
+};
 
 // const result = getTasksList(baseUrl);
 // console.log(result);
