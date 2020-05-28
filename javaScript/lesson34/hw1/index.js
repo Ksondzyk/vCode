60b56978425524d95ae762d5e2510cb51f336e0b
Ksondzyk <roman.kson@gmail.com>
2020-05-28 14:44:55 +0300

    get post put

--- javaScript/lesson34/hw1/index.js
+++ javaScript/lesson34/hw1/index.js
@@ -1,14 +1,58 @@
-var text = "demo argo";
+const baseUrl = "https://5ece5f5561c8480016701459.mockapi.io/api/v1/users";
 
-function useless(ninjaCallbacks) {
-  return ninjaCallbacks;
-}
-function getText() {
-  return text;
-}
+export const getTasksList = () => {
+  return fetch(baseUrl).then((response) => response.json());
+};
+// getTasksList();
 
-const assert =
-  (useless(function () {
-    return text;
-  }) === text,
-  "the useless function works!" + text);
+export const getUserById = (userId) => {
+  return fetch(`${baseUrl}/${userId}`).then((response) => response.json());
+};
+// getUserById("12");
+
+export const createUser = (createUser) => {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(createUser),
+  }).then((response) => response.json());
+};
+// createUser({
+//   id: "1",
+//   email: "cool@email.com",
+//   firstName: "Iron",
+//   lastName: "Man",
+//   age: 42,
+// });
+
+export const updateUser = (userId, userObject) => {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(userObject),
+  }).then((response) => response.json());
+};
+// updateUser("1", {
+//   id: "1",
+//   email: "roman@email.com",
+//   firstName: "Roman",
+//   lastName: "Kson",
+//   age: 26,
+// });
+
+export const deleteUser = (userId) => {
+  fetch(`${baseUrl}/${userId}`, { method: "DELETE" }).then((response) =>
+    response.json()
+  );
+};
+// deleteUser("1");
+// создать правильный URl c пользователями
+//  2 сделать запрос вернуть json c users
+//  3 вернуть  json с юзером
+//  4 создать пользователя на сервере
+//  5 изменить данные о пользователе
+// 6 удалить пользователя
