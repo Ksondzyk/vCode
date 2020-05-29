004c5b3f4b1317af82adac16cf81ed78d9bf415b
Ksondzyk <roman.kson@gmail.com>
2020-05-29 19:06:10 +0300

    push

--- javaScript/lesson34/hw2/index.js
+++ javaScript/lesson34/hw2/index.js
@@ -1,22 +1,5 @@
 const baseUrl = "https://5ece5f5561c8480016701459.mockapi.io/api/v1/users";
 
-// const createUser = (createUser) => {
-//   return fetch(baseUrl, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json;charset=utf-8",
-//     },
-//     body: JSON.stringify(createUser),
-//   }).then((response) => response.json());
-// };
-// createUser({
-//   id: "1",
-//   email: "cool@email.com",
-//   firstName: "Iron",
-//   lastName: "Man",
-//   age: 42,
-// });
-
 // 1. получить элементы из DOM
 // 2. создать функцию-обработчик, которая будет запущена после отправки
 // 3. собрать всю информацию о форме с помощью объекта FormData
@@ -45,36 +28,41 @@ const baseUrl = "https://5ece5f5561c8480016701459.mockapi.io/api/v1/users";
 
 // 1. получить элементы из DOM
 // 2. создать функцию-обработчик, которая будет запущена после отправки
-// 3. собрать всю информацию о форме с помощью объекта деструкрурилазции и забиси в обьект
-// 3.1 после клика по кнопке  вытягиваю значения с  инпутов и возвращаю новый масив
-// 3.2 делаю дестуктуризацию масива и записываю все значения в json обьенкт
+// 3. собрать всю информацию о форме с помощью объекта FormData
 // 4. отправить запрос на сервер с информацией формы и обработать ошибку
-// 4.1 значенияпередаю значения из предыдущей ф-ции и отправляю на сервер
-// 4.2 вывожу в консоль
 // 5. создать функцию-обработчик B, которая будет срабатывать после изменения входов
 // 6. используйте reportValidity для проверки формы
 
-const elementForm = document.querySelector("#myForm");
+const elementForm = document.querySelector(".login-form");
+elementForm.addEventListener("submit", runAfterSending);
 function runAfterSending(event) {
-  console.log(event);
   event.preventDefault();
-  const inputField = document.querySelectorAll(".form-input");
-  const valueInputField = Array.from(inputField).map((el) => el.value);
-  const [emailValue, nameValue, passwordValue] = valueInputField;
-  const obj = {
-    Email: emailValue,
-    UserName: nameValue,
-    Password: passwordValue,
-  };
+  const inputField = document.querySelector(".login-form");
+  const formData = [...new FormData(inputField)];
+  const objectForm = Object.fromEntries(formData);
+  // console.log(objectForm);
 
   fetch(baseUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
-    body: JSON.stringify(obj),
+    body: JSON.stringify(objectForm),
   })
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => alert(JSON.stringify(data)))
+    .catch(() => {
+      const errorElem = document.querySelector(".error-text");
+      errorElem.textContent = "Failed to create user";
+    });
+  inputField.reset();
 }
-elementForm.addEventListener("submit", runAfterSending);
+
+// const buttonElement = document.querySelector(".submit-button");
+
+// function clearFormFields(event) {
+//   const clearFieldInput = document.querySelector(".login-form");
+//   clearFieldInput.reset();
+// }
+
+// buttonElement.addEventListener("sumbit", clearFormFields);
