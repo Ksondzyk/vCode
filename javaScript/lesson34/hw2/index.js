b3e00c03afd5f58671309ca4058611df41d2398d
Ksondzyk <roman.kson@gmail.com>
2020-05-29 14:44:39 +0300

    push

--- javaScript/lesson34/hw2/index.js
+++ javaScript/lesson34/hw2/index.js
@@ -44,47 +44,35 @@ const baseUrl = "https://5ece5f5561c8480016701459.mockapi.io/api/v1/users";
 const buttonElem = document.querySelector(".submit-button");
 
 const handleform = () => {
-  const inputField = document.querySelectorAll(".form-input");
-  const valueInputField = Array.from(inputField).map((el) => el.value);
-  const [emailValue, nameValue, passwordValue] = valueInputField;
-
-  const formData = new FormData();
-  formData.append("email", emailValue);
-  formData.append("UserName", nameValue);
-  formData.append("password", passwordValue);
-
-  const request = new XMLHttpRequest();
-  request.open("POST", baseUrl);
-  request.send(formData);
-
-  request.open("GET", baseUrl);
-  request.send();
-
-  request.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      console.log(JSON.parse(this.responseText));
-    }
+  const dataInput = () => {
+    const inputField = document.querySelectorAll(".form-input");
+    const valueInputField = Array.from(inputField).map((el) => el.value);
+    const [emailValue, nameValue, passwordValue] = valueInputField;
+    const formInformation = {
+      email: emailValue,
+      UserName: nameValue,
+      password: passwordValue,
+    };
+    return formInformation;
   };
-
-  //   const formInformation = JSON.stringify({
-  //     email: emailValue,
-  //     name: nameValue,
-  //     password: passwordValue,
-  //   });
-  //   return formInformation;
+  dataInput();
+  const formInformationObj = dataInput();
+  const createTask = (dateObj) => {
+    return fetch(baseUrl, {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify(dateObj),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+  createTask(formInformationObj);
 };
-const formInformationObj = handleform();
-
-// const createTask = (taskData) => {
-//   return fetch(baseUrl, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json;charset=utf-8",
-//     },
-//     body: JSON.stringify(taskData),
-//   }).then((data) => console.log(data));
-// .then((response) => response.json())
-// };
-// createTask(formInformationObj);
-
 buttonElem.addEventListener("click", handleform);
