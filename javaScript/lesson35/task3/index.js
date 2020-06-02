6a0159248504b55d219e17b51ddd495d975a3103
Ksondzyk <roman.kson@gmail.com>
2020-06-02 16:37:18 +0300

    push

--- javaScript/lesson35/task3/index.js
+++ javaScript/lesson35/task3/index.js
@@ -4,7 +4,7 @@ import { renderRepos, cleanReposList } from "./repos.js";
 import { showSpinner, hideSpinner } from "./spinner.js";
 
 const defaltUser = {
-  avatar_url: "https://avatars.githubusercontent.com/u10001",
+  avatar_url: "https://avatars3.githubusercontent.com/u10001",
   name: "",
   location: "",
 };
