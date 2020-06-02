6e163146336b8930c892e5b0453ce991a8c8447e
Ksondzyk <roman.kson@gmail.com>
2020-06-02 23:08:44 +0300

    push

--- javaScript/lesson36/hw1/index.js
+++ javaScript/lesson36/hw1/index.js
@@ -1,23 +1,29 @@
 // input string
 //output promice
-export const getUsersBlogs = async (userId) => {
-  const [google, facebook, gaearon] = userId;
-  const googleBlogs = fetch(`https://api.github.com/users/${google}`)
-    .then((response) => response.json())
-    .then((users) => users.blog);
+export const getUsersBlogs = async (usersId) => {
+  console.log(usersId);
+  usersId.forEach((userId) => {
+    const googleBlogs = fetch(`https://api.github.com/users/${userId}`)
+      .then((response) => response.json())
+      .then((users) => users.blog)
+      .then((response) => console.log(response))
+      .catch((err) => new Error(err));
+  });
+  // const googleBlogs = fetch(`https://api.github.com/users/${google}`)
+  //   .then((response) => response.json())
+  //   .then((users) => users.blog);
 
-  const facebookBlogs = fetch(`https://api.github.com/users/${facebook}`)
-    .then((response) => response.json())
-    .then((users) => users.blog);
+  // const facebookBlogs = fetch(`https://api.github.com/users/${facebook}`)
+  //   .then((response) => response.json())
+  //   .then((users) => users.blog);
 
-  const gaearonBlogs = fetch(`https://api.github.com/users/${gaearon}`)
-    .then((response) => response.json())
-    .then((users) => users.blog);
+  // const gaearonBlogs = fetch(`https://api.github.com/users/${gaearon}`)
+  //   .then((response) => response.json())
+  //   .then((users) => users.blog);
 
-  const promiseArr = Promise.all([googleBlogs, facebookBlogs, gaearonBlogs]);
-  return promiseArr;
+  // const promiseArr = await [googleBlogs, facebookBlogs, gaearonBlogs];
+  // return promiseArr.map((user) => user.json());
 };
-const result = getUsersBlogs(["google", "facebook", "gaearon"]).then((data) =>
-  console.log(data)
-);
+const result = getUsersBlogs(["google", "facebook", "gaearon"]);
+
 // console.log(result);
