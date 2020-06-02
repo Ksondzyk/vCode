a28e9138b2e65e742b84dc5dff1edb35abd0a913
Ksondzyk <roman.kson@gmail.com>
2020-06-02 23:19:54 +0300

    push

--- javaScript/lesson36/hw1/index.js
+++ javaScript/lesson36/hw1/index.js
@@ -2,13 +2,19 @@
 //output promice
 export const getUsersBlogs = async (usersId) => {
   console.log(usersId);
-  usersId.forEach((userId) => {
-    const googleBlogs = fetch(`https://api.github.com/users/${userId}`)
-      .then((response) => response.json())
-      .then((users) => users.blog)
-      .then((url) => fetch(url))
-      .then((response) => console.log(response));
-  });
+  // usersId.forEach((userId) => {
+  //   const googleBlogs = fetch(`https://api.github.com/users/${userId}`)
+  //     .then((response) => response.json())
+  //     .then((users) => users.blog)
+  //     .then((url) => fetch(url))
+  //     .then((response) => console.log(response));
+  // });
+
+  let requests = usersId.map((url) => fetch(url.blog));
+
+  // Promise.all будет ожидать выполнения всех промисов
+  Promise.all(requests).then((responses) =>
+    responses.forEach((response) => response)
   // const googleBlogs = fetch(`https://api.github.com/users/${google}`)
   //   .then((response) => response.json())
   //   .then((users) => users.blog);
