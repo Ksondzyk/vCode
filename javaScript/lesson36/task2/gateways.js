318c5e84baf26df674bfd7a80cf9fd7c521a362f
Ksondzyk <roman.kson@gmail.com>
2020-06-02 18:35:05 +0300

    push

---
+++ javaScript/lesson36/task2/gateways.js
@@ -0,0 +1,14 @@
+export const fetchUserDate = async (userName) => {
+  const response = await fetch(`https://api.github.com/users/${userName}`);
+  if (response.ok) {
+    return await response.json();
+  }
+  throw new Error("Failed to load data");
+};
+export const fetchRepositoris = async (url) => {
+  const response = await fetch(url);
+  if (response.ok) {
+    return await response.json();
+  }
+  throw new Error("Failed to load data");
+};
---
+++ javaScript/lesson36/task2/index.js
@@ -0,0 +1,48 @@
+import { fetchUserDate, fetchRepositoris } from "./gateways.js";
+import { renderUserData } from "./user.js";
+import { renderRepos, cleanReposList } from "./repos.js";
+import { showSpinner, hideSpinner } from "./spinner.js";
+
+const defaltUser = {
+  avatar_url: "https://avatars3.githubusercontent.com/u10001",
+  name: "",
+  location: "",
+};
+
+renderUserData(defaltUser);
+
+const showUserBtnElem = document.querySelector(".name-form__btn");
+const userNameInputElem = document.querySelector(".name-form__input");
+
+const onSearchUser = async () => {
+  showSpinner();
+  cleanReposList();
+  const userName = userNameInputElem.value;
+  try {
+    const userData = await fetchUserDate(userName);
+    renderUserData(userData);
+    const reposList = await fetchRepositoris(userData.repos_url);
+    renderRepos(reposList);
+  } catch (err) {
+    alert(err.message);
+  } finally {
+    hideSpinner();
+  }
+  // fetchUserDate(userName)
+  //   .then((userData) => {
+  //     renderUserData(userData);
+  //     return userData.repos_url;
+  //   })
+  //   .then((url) => fetchRepositoris(url))
+  //   .then((reposList) => {
+  //     renderRepos(reposList);
+  //   })
+  //   .catch((err) => {
+  //     alert(err.message);
+  //   })
+  //   .finally(() => {
+  //     hideSpinner();
+  //   });
+};
+
+showUserBtnElem.addEventListener("click", onSearchUser);
