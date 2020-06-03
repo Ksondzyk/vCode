e8477b1c0dbf327e23cfabb97e24b3abc793c87a
Ksondzyk <roman.kson@gmail.com>
2020-06-03 20:34:31 +0300

    push

---
+++ javaScript/lesson37/hw1/index.js
@@ -0,0 +1,14 @@
+// input string
+//output promice
+export const getUsersBlogs = async (usersId) => {
+  let requests = usersId.map((url) =>
+    fetch(`https://api.github.com/users/${url}`).then((response) =>
+      response.json()
+    )
+  );
+  const result = await Promise.all(requests);
+  return result.map((user) => user.blog);
+};
+const result = getUsersBlogs(["google", "facebook", "gaearon"]);
+
+// console.log(result);
